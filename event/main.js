30f53cc970865568696dc632dc5cf3400a4d038c
YaroslavW <doctor355@yahoo.com>
2018-12-01 13:27:42 +0300

    event onclick

--- event/main.js
+++ event/main.js
@@ -1,2 +1,15 @@
 btn.onclick=()=>{alert('BTN')}
 
+function Hello(){
+	alert("Hello");
+}
+
+// button.onclick= Hello(); Immediately upon page loading
+button.onclick= Hello; //on click
+
+// with_this.onclick = function(){
+// 	alert(this.innerHTML);
+// } 
+//This
+
+with_this.onclick= () => {alert(this.innerHTML)} //undefined
