02b1c120fa77b70640d6cd7dc343638f03972614
YaroslavW <doctor355@yahoo.com>
2019-01-06 16:57:19 +0300

    let-scope

--- es6/let-const/variable-scope.js
+++ es6/let-const/variable-scope.js
@@ -1,9 +1,21 @@
-var apples = 5;
+// var apples = 5;
+
+// if (true) {
+//   var apples = 10;
+
+//   console.log(apples); 
+//   // 10 (внутри блока)
+// }
+
+// console.log(apples); 
+// // 10 (снаружи блока то же самое)
+
+let apples = 5; // (*)
 
 if (true) {
-  var apples = 10;
+  let apples = 10;
 
   console.log(apples); // 10 (внутри блока)
 }
 
-console.log(apples); // 10 (снаружи блока то же самое)
+console.log(apples); // 5 (снаружи блока значение не изменилось)
