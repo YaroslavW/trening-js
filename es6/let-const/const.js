0f2e95c5b299b2b9df7a4812a639c4f6da434abc
YaroslavW <doctor355@yahoo.com>
2019-01-07 13:52:50 +0300

    Const

---
+++ es6/let-const/const.js
@@ -0,0 +1,15 @@
+// const apple = 5;
+// apple = 10;
+// console.log(apple);
+// Uncaught TypeError: Assignment to constant variable.
+
+
+const user = {
+    name: "John"
+  };
+  
+  user.name = "Sarah";
+  console.log(user.name); 
+//   Sarah
+user = 5;
+console.log(user); 
