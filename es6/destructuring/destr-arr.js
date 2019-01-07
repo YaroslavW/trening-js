da108569bc6a762e35f9b70c615685e36372073a
YaroslavW <doctor355@yahoo.com>
2019-01-07 14:18:08 +0300

    Spread operator

--- es6/destructuring/destr-arr.js
+++ es6/destructuring/destr-arr.js
@@ -5,6 +5,14 @@
 // console.log(lastName);
 // // Kolesnikov
 
-let [,,,,,,, language] = "Brendan Eich  is creator of the JavaScript programming language.".split(" ");
+// let [ ,, language] = "Brendan Eich  is creator of the JavaScript programming language.".split(" ");
 
-console.log(language);
+// console.log(language);
+
+// ================SPREAD==================
+
+let [lastName, firstName, ...rest] = "Brendan Eich  is creator of the JavaScript programming language.".split(" ");
+
+console.log(firstName);
+console.log(lastName);
+console.log(rest);
