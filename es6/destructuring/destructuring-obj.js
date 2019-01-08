bbb018ae4af7ac0d0e8788d2e4ffeb41420525a5
YaroslavW <doctor355@yahoo.com>
2019-01-08 20:18:09 +0300

    Destructuring Objects

---
+++ es6/destructuring/destructuring-obj.js
@@ -0,0 +1,12 @@
+let options = {
+    title: 'menu',
+    width: 100,
+    height: 200,
+};
+
+let { title, width, height } = options;
+
+console.log(` title = ${title}; width = ${width}; height = ${height};`);
+// title = menu; width = 100; height = 200;
+
+// Change property name 
