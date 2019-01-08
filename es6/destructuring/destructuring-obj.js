8551ccaefb39a5c7c3875875a4ba1309f2e1faae
YaroslavW <doctor355@yahoo.com>
2019-01-08 20:21:45 +0300

    Change property name when assigned

--- es6/destructuring/destructuring-obj.js
+++ es6/destructuring/destructuring-obj.js
@@ -4,9 +4,12 @@ let options = {
     height: 200,
 };
 
-let { title, width, height } = options;
+// let { title, width, height } = options;
 
-console.log(` title = ${title}; width = ${width}; height = ${height};`);
+// console.log(` title = ${title}; width = ${width}; height = ${height};`);
 // title = menu; width = 100; height = 200;
 
-// Change property name 
+// Change property name at assignment
+
+let {title: name, width: w, height: h} = options;
+console.log(` name = ${name}; w = ${w}; h = ${h};`);
