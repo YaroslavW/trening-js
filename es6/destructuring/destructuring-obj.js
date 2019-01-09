8e9eb06b6ed8de8471396406d76ae95eee03fb2d
YaroslavW <doctor355@yahoo.com>
2019-01-09 14:42:58 +0300

    destructuring object and default property values

--- es6/destructuring/destructuring-obj.js
+++ es6/destructuring/destructuring-obj.js
@@ -11,5 +11,10 @@ let options = {
 
 // Change property name at assignment
 
-let {title: name, width: w, height: h} = options;
-console.log(` name = ${name}; w = ${w}; h = ${h};`);
+// let {title: name, width: w, height: h} = options;
+// console.log(` name = ${name}; w = ${w}; h = ${h};`);
+
+// destructuring and default property values
+
+let { title, width: w, color='red'} = options;
+console.log(` title = ${title}; w = ${w}; color = ${color};`);
