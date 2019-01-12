3135b863222e14c4562cfc026ab0887d5f41d572
YaroslavW <doctor355@yahoo.com>
2019-01-12 18:20:36 +0300

    transferring an array of parameters as a list

--- es6/functions/spread.js
+++ es6/functions/spread.js
@@ -1,6 +1,10 @@
-function showName(firstName, lastName, ...rest){
-    console.log(`First Name = ${firstName}; Last Name = ${lastName}; Rest = ${rest}`)
-};
+// function showName(firstName, lastName, ...rest){
+//     console.log(`First Name = ${firstName}; Last Name = ${lastName}; Rest = ${rest}`)
+// };
 
-showName('Yaroslav', 'Kolesnikov','studying', 'java', 'script');
-// First Name = Yaroslav; Last Name = Kolesnikov; Rest = studying,java,script
+// showName('Yaroslav', 'Kolesnikov','studying', 'java', 'script');
+// // First Name = Yaroslav; Last Name = Kolesnikov; Rest = studying,java,script
+
+let arrayNumbers = [3, 9, 1, 35];
+let maxNumber = Math.max(...arrayNumbers);
+console.log(maxNumber); //35
