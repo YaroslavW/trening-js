3d14c5f8c7a61501cc4e56f4600918cb84bd8a47
YaroslavW <doctor355@yahoo.com>
2019-01-12 18:09:10 +0300

    values as expressionsЭ

--- es6/functions/default_settings.js
+++ es6/functions/default_settings.js
@@ -1,5 +1,9 @@
-function showMenu(title='Menu', width=300, height=200){
-    console.log(`Title = ${title}; Width = ${width}; Height = ${height}`);
+function showMenu(title='Menu', width=300, height=200 ,who = getName().toUpperCase()){
+    console.log(`Title = ${title}; Width = ${width}; Height = ${height} Who = ${who}`);
+}
+
+function getName(){
+    return 'yaroslav';
 }
 showMenu();
 // Title = Menu; Width = 300; Height = 200
