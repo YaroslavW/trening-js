8bf6b910570bf91e6ddfa80e559783c05e4d8028
YaroslavW <doctor355@yahoo.com>
2019-01-13 14:50:22 +0300

    Name functions

---
+++ es6/functions/property-name.js
@@ -0,0 +1,10 @@
+function y(){};
+
+let u = function(){};
+
+let user = {
+    sayHay: function(){}
+}
+console.log(`Name first function y = ${y.name} and name second function u = ${u.name} name third function = ${user.sayHay.name}`);
+// Name first function y = y and name second function u = u
+
