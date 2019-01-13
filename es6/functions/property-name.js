e1f0750644e888855e2ff7990e2b96b2c6e67060
YaroslavW <doctor355@yahoo.com>
2019-01-13 14:57:38 +0300

    function inside block

--- es6/functions/property-name.js
+++ es6/functions/property-name.js
@@ -1,10 +1,19 @@
-function y(){};
+// function y(){};
 
-let u = function(){};
+// let u = function(){};
 
-let user = {
-    sayHay: function(){}
+// let user = {
+//     sayHay: function(){}
+// }
+// console.log(`Name first function y = ${y.name} and name second function u = ${u.name} name third function = ${user.sayHay.name}`);
+//Name first function y = y and name second function u = u name third function = sayHay
+'use strict';
+if( true ) {
+    sayHi();
+    function sayHi(){
+        console.log('Hi');
+    }
 }
-console.log(`Name first function y = ${y.name} and name second function u = ${u.name} name third function = ${user.sayHay.name}`);
-// Name first function y = y and name second function u = u
-
+sayHi();
+// with 'use strict' last sayHi - ReferenceError: sayHi is not defined
+// without 'use strict' - all works normal
