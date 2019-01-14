fe1e69b432a344a0facbc2d1124674337ec83aba
YaroslavW <doctor355@yahoo.com>
2019-01-14 11:39:21 +0300

    arrow func without arg

--- es6/functions/arrow-function.js
+++ es6/functions/arrow-function.js
@@ -1,4 +1,12 @@
+// one arg
 let inc = x => x + 1;
 
 console.log(inc(10)); //11
+// more one arg
+let sum = (a, b) => a + b;
 
+console.log( sum( 5, 3 )); // 8
+
+// without arg
+let getTime = () => new Date().getHours() + ' : ' + new Date().getMinutes();
+console.log(getTime()); // current time
