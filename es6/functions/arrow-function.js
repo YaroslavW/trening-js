d311667b22ee591e5fdc1e5bc48990f8f072c149
YaroslavW <doctor355@yahoo.com>
2019-01-14 11:49:34 +0300

    many lines of function

--- es6/functions/arrow-function.js
+++ es6/functions/arrow-function.js
@@ -8,5 +8,15 @@ let sum = (a, b) => a + b;
 console.log( sum( 5, 3 )); // 8
 
 // without arg
-let getTime = () => new Date().getHours() + ' : ' + new Date().getMinutes();
+// let getTime = () => new Date().getHours() + ' : ' + new Date().getMinutes();
+// console.log(getTime()); // current time
+
+
+// many str in func
+let getTime = () => {
+    let date = new Date();
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    return hour + ' : ' + min;
+}
 console.log(getTime()); // current time
