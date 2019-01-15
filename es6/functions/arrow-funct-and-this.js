6733fcf6b28b74f9163bd6affe9ddb25c91d5ac7
YaroslavW <doctor355@yahoo.com>
2019-01-15 11:34:39 +0300

    Arrow finction and this

---
+++ es6/functions/arrow-funct-and-this.js
@@ -0,0 +1,30 @@
+'use strict';
+// Inside the shooter functions, this is the same as the outside.
+// let group = {
+//     title: 'Our course',
+//     students: ['John','Sarah','Bill'],
+
+//     showList: function() {
+//         this.students.forEach(
+//             student => console.log(this.title + ' : ' + student)
+//         )
+//     }
+// }
+// group.showList();
+// Our course : John
+// Our course : Sarah
+// Our course : Bill
+
+let group = {
+    title: 'Our course',
+    students: ['John','Sarah','Bill'],
+
+    showList: function() {
+        this.students.forEach(function(student) {
+          console.log(this.title + ': ' + student); // будет ошибка
+        })
+      }
+}
+group.showList();
+// Errror
+// this внутри forEach будет undefined.
