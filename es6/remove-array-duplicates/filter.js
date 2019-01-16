d5dc6775b2cbfce6d0a8bd98fd7fd7d5f250af91
YaroslavW <doctor355@yahoo.com>
2019-01-16 15:19:31 +0300

    Return array duplicate Fiter - all items in loop

---
+++ es6/remove-array-duplicates/filter.js
@@ -0,0 +1,17 @@
+const array = ['dog', 1, 1, 2, 'dog', 3];
+
+array.filter( (item, index) => {
+    console.log(
+        item,
+        index,
+        array.indexOf(item),
+        array.indexOf(item) === index
+    );
+    return array.indexOf(item) === index;
+});
+// dog 0 0 true
+// 1 1 1 true
+// 1 2 1 false
+// 2 3 3 true
+// dog 4 0 false
+// 3 5 5 true
