49912dac818b52ba1730dcf92b6c17ccdbf15d2d
YaroslavW <doctor355@yahoo.com>
2019-01-16 15:34:13 +0300

    remove array duplicates with reduce

---
+++ es6/remove-array-duplicates/reduce.js
@@ -0,0 +1,17 @@
+const array = ['dog', 1, 1, 2, 'dog', 3];
+
+array.reduce( (unique, item) => {
+    console.log(
+        item,
+        unique,
+        unique.includes(item),
+        unique.includes(item) ? unique : [ ...unique, item ]
+    );
+    return  unique.includes(item) ? unique : [ ...unique, item ]
+},[]);
+// dog [] false [ 'dog' ]
+// 1 [ 'dog' ] false [ 'dog', 1 ]
+// 1 [ 'dog', 1 ] true [ 'dog', 1 ]
+// 2 [ 'dog', 1 ] false [ 'dog', 1, 2 ]
+// dog [ 'dog', 1, 2 ] true [ 'dog', 1, 2 ]
+// 3 [ 'dog', 1, 2 ] false [ 'dog', 1, 2, 3 ]
