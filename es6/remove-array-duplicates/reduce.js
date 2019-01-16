f6118eb056446ed1fd67d1da77dcc47f1231dd5c
YaroslavW <doctor355@yahoo.com>
2019-01-16 15:35:48 +0300

    one string with reduce

--- es6/remove-array-duplicates/reduce.js
+++ es6/remove-array-duplicates/reduce.js
@@ -1,12 +1,12 @@
 const array = ['dog', 1, 1, 2, 'dog', 3];
 
-array.reduce( (unique, item) => {
-    console.log(
-        item,
-        unique,
-        unique.includes(item),
-        unique.includes(item) ? unique : [ ...unique, item ]
-    );
+const uniq = array.reduce( (unique, item) => {
+    // console.log(
+    //     item,
+    //     unique,
+    //     unique.includes(item),
+    //     unique.includes(item) ? unique : [ ...unique, item ]
+    // );
     return  unique.includes(item) ? unique : [ ...unique, item ]
 },[]);
 // dog [] false [ 'dog' ]
@@ -15,3 +15,5 @@ array.reduce( (unique, item) => {
 // 2 [ 'dog', 1 ] false [ 'dog', 1, 2 ]
 // dog [ 'dog', 1, 2 ] true [ 'dog', 1, 2 ]
 // 3 [ 'dog', 1, 2 ] false [ 'dog', 1, 2, 3 ]
+
+console.log(uniq);
