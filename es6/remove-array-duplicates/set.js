5ef472411da98578cd8a581987ded488356bf144
YaroslavW <doctor355@yahoo.com>
2019-01-16 15:01:51 +0300

    Set as one string

--- es6/remove-array-duplicates/set.js
+++ es6/remove-array-duplicates/set.js
@@ -1,7 +1,13 @@
 const array = ['cat', 1, 2, 'cat', 'cat', 3];
 
-const uniqueSet = new Set( array );
-console.log(uniqueSet);
-// Set { 'cat', 1, 2, 3 }
-const backToArray = [...uniqueSet];
-console.log(backToArray);
+// const uniqueSet = new Set( array );
+// console.log(uniqueSet);
+// // Set { 'cat', 1, 2, 3 }
+
+// const backToArray = [...uniqueSet];
+// console.log(backToArray);
+// // [ 'cat', 1, 2, 3 ]
+
+const unique = Array.from(new Set(array));
+console.log(unique); 
+// [ 'cat', 1, 2, 3 ]
