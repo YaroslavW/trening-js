6c3b4b0fd24d38cc640746d592b621054133ff22
YaroslavW <doctor355@yahoo.com>
2019-01-17 14:22:59 +0300

    output date on the page

--- output-date-range/common.js
+++ output-date-range/common.js
@@ -3,11 +3,12 @@
 document.querySelector('button').addEventListener('click',function(){
     let startDate = document.getElementById('start-date').value;
     let endDate = document.getElementById('end-date').value;
+    let output = document.getElementById('output');
     startDate = Date.parse(startDate);
     endDate = Date.parse(endDate);
 
     for( let i = startDate; i <= endDate; i = i + 24 * 60 * 60 * 1000){
-        console.log(i);
+        output.innerHTML += new Date(i).toISOString().substr(0,10) + '<br />';
     }
     // console.log(startDate, endDate);
 });
