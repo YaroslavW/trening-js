ff4a38f90e373944737dcc8711f7c71cd1a83c3a
YaroslavW <doctor355@yahoo.com>
2019-01-17 14:14:27 +0300

    output date range in milliseconds

--- output-date-range/common.js
+++ output-date-range/common.js
@@ -5,7 +5,11 @@ document.querySelector('button').addEventListener('click',function(){
     let endDate = document.getElementById('end-date').value;
     startDate = Date.parse(startDate);
     endDate = Date.parse(endDate);
-    console.log(startDate, endDate);
+
+    for( let i = startDate; i <= endDate; i = i + 24 * 60 * 60 * 1000){
+        console.log(i);
+    }
+    // console.log(startDate, endDate);
 });
 
 // https://www.epochconverter.com/ - good site for checking convertion date
