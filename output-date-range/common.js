5b3374412d84df04a42626900c5e1f8eaea10cd6
YaroslavW <doctor355@yahoo.com>
2019-01-17 14:09:53 +0300

    date conversion to numeric format

--- output-date-range/common.js
+++ output-date-range/common.js
@@ -3,5 +3,10 @@
 document.querySelector('button').addEventListener('click',function(){
     let startDate = document.getElementById('start-date').value;
     let endDate = document.getElementById('end-date').value;
+    startDate = Date.parse(startDate);
+    endDate = Date.parse(endDate);
     console.log(startDate, endDate);
-})
+});
+
+// https://www.epochconverter.com/ - good site for checking convertion date
+
