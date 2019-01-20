8a0ace7a655af7ac08dcc4044282d086dcf08174
YaroslavW <doctor355@yahoo.com>
2019-01-20 19:40:47 +0300

    add key event

--- JS-for-site/adopt-menu-js/common.js
+++ JS-for-site/adopt-menu-js/common.js
@@ -1,6 +1,12 @@
 window.onload = function(){
     document.querySelector('#menu').onmouseover = menuShow;
     document.querySelector('#menu').onmouseout = menuHide;
+
+    document.onkeypress = function (event) {
+        console.log( event );
+        if ( event.code == "KeyM")  menuShow();
+    }
+
     function menuShow(){
         document.querySelector('#menu').style.left = 0;
     }
