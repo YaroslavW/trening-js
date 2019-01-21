5596cb088696adb303e40dc92666c3b7a4a7e133
YaroslavW <doctor355@yahoo.com>
2019-01-21 15:26:45 +0300

    Nesting templates

--- es6/template-literals/mult-expr-nesting.js
+++ es6/template-literals/mult-expr-nesting.js
@@ -13,3 +13,10 @@ console.log(`Summ ${a} + ${b} = ${a + b}`); //Summ 3 + 6 = 9
 
 console.log(`Larger number ${a < b ?  b : a}`); //Larger number 6
 
+const condition = function(){
+    return false;
+}
+
+console.log(`some ${condition() ? 'true': `box-${a<b ? '3<6':'3>6'}`}`); 
+// if condition = true ->some true
+// if condition = false ->some box-3<6
