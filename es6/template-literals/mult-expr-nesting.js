e215587d5369fae652594b4fb30537fbfb92ca49
YaroslavW <doctor355@yahoo.com>
2019-01-21 15:17:33 +0300

    Expression interpolation

--- es6/template-literals/mult-expr-nesting.js
+++ es6/template-literals/mult-expr-nesting.js
@@ -6,3 +6,10 @@ console.log(str1);
 // It is one line
 // and
 // it is third line
+
+const a = 3;
+const b = 6;
+console.log(`Summ ${a} + ${b} = ${a + b}`); //Summ 3 + 6 = 9
+
+console.log(`Larger number ${a < b ?  b : a}`); //Larger number 6
+
