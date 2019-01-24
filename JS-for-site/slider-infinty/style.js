8bae715faafbc08e7d11b27ff066813e65d224ff
YaroslavW <doctor355@yahoo.com>
2019-01-24 18:10:25 +0300

    output one img

--- JS-for-site/slider-infinty/style.js
+++ JS-for-site/slider-infinty/style.js
@@ -7,3 +7,16 @@ for ( let i = 0; i < slides.length; i++ ) {
     slides[i].remove();
 }
 console.log(slider);//arr with img-src
+
+let step = 0;
+let offset = 0;
+
+function draw(){
+    let img = document.createElement('img');
+    img.src = slider[step];
+    img.classList.add('slide-single');
+    img.style.left = offset * 400 + 'px';
+    document.getElementById('slider').appendChild(img);
+}
+
+draw();
