deef71f18de6894db2759c032e7a7235bf26a415
YaroslavW <doctor355@yahoo.com>
2019-01-25 13:28:41 +0300

    Default Parameters in ES6

---
+++ es6/top-ES-features/default-parametrs.js
@@ -0,0 +1,12 @@
+let sum = (x = 2, y = 3) => x + y;
+
+console.log(sum(4, 5)); //9
+console.log(sum()); //5
+
+// ES6
+var calculateArea = function(height = 50, width = 80) {  
+    // write logic
+    ...
+}
+
+// ES5
