25e0179e9982c5ec2304c73ae5f21df3c1d35be4
YaroslavW <doctor355@yahoo.com>
2019-01-27 13:54:57 +0300

    array from

---
+++ es6/top-ES-features/helpers-func/array-from.js
@@ -0,0 +1,19 @@
+let str = 'Hello';
+let arr = Array.from(str);
+console.log(arr); //[ 'H', 'e', 'l', 'l', 'o' ]
+console.log(arr[1]); // e
+
+let arrNum = Array.from([1, 2, 3]); 
+
+console.log(arrNum);  // [1, 2, 3]
+
+let namesSet = new Set(['jamie', 'jack']);
+let arrStr = Array.from(namesSet); 
+
+console.log(arrStr); // ['jamie', 'jack']
+
+let arrNumMult = Array.from([1, 2, 3], x => x * x);
+console.log(arrNumMult) // [1, 4, 9]
+
+let arrKey = Array.from({ length: 4 }, (val, key) => key);
+console.log(arrKey); // [0, 1, 2, 3]
