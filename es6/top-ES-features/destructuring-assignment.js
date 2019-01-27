f35fc516ac07442fca823b829da4ea137697ee82
YaroslavW <doctor355@yahoo.com>
2019-01-27 14:05:13 +0300

    Destructuring assignment with arrays

--- es6/top-ES-features/destructuring-assignment.js
+++ es6/top-ES-features/destructuring-assignment.js
@@ -19,3 +19,31 @@ var movieStar = {
   
   // James Bond
   // Federal Agent
+
+ //WITH ARRAY
+ 
+ var mortalKombat = ['Scorpion', 'Liu Kang', 'Sub Zero', 'Johnny Cage'];
+
+console.log(mortalKombat[0]);
+// Scorpion
+
+let mortalKombat1 = ['Scorpion', 'Liu Kang', 'Sub Zero', 'Johnny Cage'];
+
+let [user1, user2] = mortalKombat1;
+
+console.log(user1, user2);
+// Scorpion
+// Liu Kang
+
+let mortalKombat2 = ['Scorpion', 'Liu Kang', 'Sub Zero', 'Johnny Cage'];
+
+let [userA, , userB] = mortalKombat2;
+let [user, ...users] = mortalKombat2;
+
+console.log(userA, userB);
+// Scorpion
+// Sub Zero
+
+console.log(user, users);
+// Scorpion
+// ['Liu Kang', 'Sub Zero', 'Johnny Cage']
