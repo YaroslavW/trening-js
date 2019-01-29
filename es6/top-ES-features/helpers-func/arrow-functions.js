c6b8880d2a568aab8a440793568b5f65d7d483e1
YaroslavW <doctor355@yahoo.com>
2019-01-29 13:45:44 +0300

    this context in detail

--- es6/top-ES-features/helpers-func/arrow-functions.js
+++ es6/top-ES-features/helpers-func/arrow-functions.js
@@ -9,3 +9,32 @@ $('.btn').click((event) =>{
     this.sendData()
   })
 
+// ES5
+var logUpperCase = function() {
+    var _this = this
+  
+    this.string = this.string.toUpperCase()
+    return function () {
+      return console.log(_this.string)
+    }
+  }
+  
+  logUpperCase.call({ string: 'es6 rocks' })()
+
+//   ES6
+var logUpperCase = function() {
+    this.string = this.string.toUpperCase()
+    return () => console.log(this.string)
+  }
+  
+  logUpperCase.call({ string: 'es6 rocks' })()
+
+
+  var ids = ['5632953c4e345e145fdf2df8','563295464e345e145fdf2df9']
+var messages = ids.map(function (value) {
+  return "ID is " + value // explicit return
+})
+
+
+var ids = ['5632953c4e345e145fdf2df8','563295464e345e145fdf2df9']
+var messages = ids.map((value, index, list) => `ID of ${index} element is ${value} `) // implicit return
