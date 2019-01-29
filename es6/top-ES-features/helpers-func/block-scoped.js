34157871bc8e19529455eb2a7223da4df46992a2
YaroslavW <doctor355@yahoo.com>
2019-01-29 13:48:50 +0300

    Block Scoped

---
+++ es6/top-ES-features/helpers-func/block-scoped.js
@@ -0,0 +1,14 @@
+// Let
+function foo() {
+    var par = 1;
+    if (par >= 0) {
+      var bar = 2;
+      console.log(par); // prints 1
+      console.log(bar); // prints 2
+    }
+    console.log(par); // prints 1
+    console.log(bar); // prints 2
+  }
+  foo();
+
+//   const
