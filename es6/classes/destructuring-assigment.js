15994d6598784fe6ab2654539b4ce17c96944cc5
YaroslavW <doctor355@yahoo.com>
2019-02-16 18:56:18 +0300

    destructuring assigment with arrays

--- es6/classes/destructuring-assigment.js
+++ es6/classes/destructuring-assigment.js
@@ -20,3 +20,33 @@ var movieStar = {
   
   // James Bond
   // Federal Agent
+
+  //==>With arrays
+  var mortalKombat = ['Scorpion', 'Liu Kang', 'Sub Zero', 'Johnny Cage'];
+
+console.log(mortalKombat[0]);
+// Scorpion
+
+
+
+let mortalKombat = ['Scorpion', 'Liu Kang', 'Sub Zero', 'Johnny Cage'];
+
+let [user1, user2] = mortalKombat;
+
+console.log(user1, user2);
+// Scorpion
+// Liu Kang
+
+
+let mortalKombat = ['Scorpion', 'Liu Kang', 'Sub Zero', 'Johnny Cage'];
+
+let [userA, , userB] = mortalKombat;
+let [user, ...users] = mortalKombat;
+
+console.log(userA, userB);
+// Scorpion
+// Sub Zero
+
+console.log(user, users);
+// Scorpion
+// ['Liu Kang', 'Sub Zero', 'Johnny Cage']
