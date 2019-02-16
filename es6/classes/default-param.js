db15b5196ed7311dc2a4037669ea18e011cddbc8
YaroslavW <doctor355@yahoo.com>
2019-02-16 18:42:00 +0300

    classes - default parameters

---
+++ es6/classes/default-param.js
@@ -0,0 +1,10 @@
+var calculateArea = function(height = 50, width = 80) {  
+    // write logic
+
+}
+
+var calculateArea = function(height, width) {  
+    height =  height || 50;
+    width = width || 80;
+   
+ }
