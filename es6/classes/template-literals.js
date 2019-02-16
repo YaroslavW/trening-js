aa9e542b1ab33185498da5dd2b23db5e45a9ef39
YaroslavW <doctor355@yahoo.com>
2019-02-16 18:52:03 +0300

    classes templates literals

---
+++ es6/classes/template-literals.js
@@ -0,0 +1,9 @@
+var firstName = 'Yaroslav';
+var lastName = 'Kolesnikov';
+
+var name = `Your name is ${firstName} ${lastName}.`
+console.log(name);
+// Your name is Yaroslav Kolesnikov.
+var name1 = 'Your name is ' + firstName + ' ' + lastName + '.';
+console.log(name1);
+// Your name is Yaroslav Kolesnikov.
