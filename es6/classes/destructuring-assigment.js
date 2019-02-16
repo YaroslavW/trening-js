2843f7f581375bfdbbec1103f0d6199089be4215
YaroslavW <doctor355@yahoo.com>
2019-02-16 18:54:42 +0300

    classes destructuring assigment

---
+++ es6/classes/destructuring-assigment.js
@@ -0,0 +1,22 @@
+var movieStar = {
+    name: 'James Bond',
+    nickname: 'Bond',
+    profession: 'Federal Agent'
+  };
+  
+  console.log(movieStar.name);
+  // James Bond
+
+
+  var movieStar = {
+    name: 'James Bond',
+    nickname: 'Bond',
+    profession: 'Federal Agent'
+  };
+  
+  let { name, profession } = movieStar;
+  
+  console.log(name, profession);
+  
+  // James Bond
+  // Federal Agent
