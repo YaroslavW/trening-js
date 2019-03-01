aa70a102953ec5f1f1f44cca90a2acad107a797a
YaroslavW <doctor355@yahoo.com>
2019-03-01 11:22:44 +0300

    Filter Method - Negating Predicates

--- es6/filter/simple_filter.js
+++ es6/filter/simple_filter.js
@@ -56,11 +56,36 @@ const restaurants = [
     },
 ];
 
-const isBurger = ({ cuisine }) => cuisine === 'Burger';
-const burgerJoints = restaurants.filter(isBurger);
+// const isBurger = ({ cuisine }) => cuisine === 'Burger';
+// const burgerJoints = restaurants.filter(isBurger);
+// console.log( burgerJoints );
 
-console.log( burgerJoints );
 // [ { name: 'Dan\'s Hamburgers', price: 'Cheap', cuisine: 'Burger' },
 //   { name: 'P. Terry\'s', price: 'Cheap', cuisine: 'Burger' },
 //   { name: 'Hopdoddy', price: 'Expensive', cuisine: 'Burger' },
 //   { name: 'Whataburger', price: 'Moderate', cuisine: 'Burger' } ]
+
+
+// ====> Negating Predicates
+// Its not DRY code
+// const isNotBurger = ({cuisine}) => cuisine !== 'Burger';
+// const burgerNotJoints = restaurants.filter(isNotBurger);
+// console.log( burgerNotJoints );
+
+
+// [ { name: 'Austin\'s Pizza', price: 'Cheap', cuisine: 'Pizza' },
+//   { name: 'Via 313', price: 'Moderate', cuisine: 'Pizza' },
+//   { name: 'Bufalina', price: 'Expensive', cuisine: 'Pizza' },
+//   { name: 'Chuy\'s', cuisine: 'Tex-Mex', price: 'Moderate' },
+//   { name: 'Taquerias Arandina',
+//     cuisine: 'Tex-Mex',
+//     price: 'Cheap' },
+//   { name: 'El Alma', cuisine: 'Tex-Mex', price: 'Expensive' },
+//   { name: 'Maudie\'s', cuisine: 'Tex-Mex', price: 'Moderate' } ]
+
+const isBurger = ({cuisine}) => cuisine === 'Burger';
+const isNotBurger = restaurant => !isBurger(restaurant);
+console.log(isNotBurger); //
+
+const burgerNotJoints = restaurants.filter(isNotBurger);
+console.log( burgerNotJoints );
