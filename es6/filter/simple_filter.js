7a6fe7b8dc2ee645c491766290e2cc3a840b70a0
YaroslavW <doctor355@yahoo.com>
2019-03-01 11:26:49 +0300

    Filter Method - Negative Predicate with right DRY method

--- es6/filter/simple_filter.js
+++ es6/filter/simple_filter.js
@@ -83,9 +83,32 @@ const restaurants = [
 //   { name: 'El Alma', cuisine: 'Tex-Mex', price: 'Expensive' },
 //   { name: 'Maudie\'s', cuisine: 'Tex-Mex', price: 'Moderate' } ]
 
-const isBurger = ({cuisine}) => cuisine === 'Burger';
-const isNotBurger = restaurant => !isBurger(restaurant);
-console.log(isNotBurger); //
+// const isBurger = ({cuisine}) => cuisine === 'Burger';
+// const isNotBurger = restaurant => !isBurger(restaurant);
+// console.log(isNotBurger); //[Function: isNotBurger]
 
-const burgerNotJoints = restaurants.filter(isNotBurger);
+// const burgerNotJoints = restaurants.filter(isNotBurger);
+// console.log( burgerNotJoints );
+// [ { name: 'Austin\'s Pizza', price: 'Cheap', cuisine: 'Pizza' },
+//   { name: 'Via 313', price: 'Moderate', cuisine: 'Pizza' },
+//   { name: 'Bufalina', price: 'Expensive', cuisine: 'Pizza' },
+//   { name: 'Chuy\'s', cuisine: 'Tex-Mex', price: 'Moderate' },
+//   { name: 'Taquerias Arandina',
+//     cuisine: 'Tex-Mex',
+//     price: 'Cheap' },
+//   { name: 'El Alma', cuisine: 'Tex-Mex', price: 'Expensive' },
+//   { name: 'Maudie\'s', cuisine: 'Tex-Mex', price: 'Moderate' } ]
+
+//===<<Right method negative predicate
+const negate = predicate => function() {
+    return !predicate.apply(null, arguments);
+  }
+  
+  const isBurger = ({cuisine}) => cuisine === 'Burger';
+  const isNotBurger = negate(isBurger);
+  
+  const isPizza = ({cuisine}) => cuisine === 'Pizza';
+  const isNotPizza = negate(isPizza);
+  console.log(isNotPizza); //[Function]
+  const burgerNotJoints = restaurants.filter(isNotPizza);
 console.log( burgerNotJoints );
