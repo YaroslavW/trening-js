e53d820df080eb93e32c71b40592a6c0de5dcf9c
YaroslavW <doctor355@yahoo.com>
2019-03-05 11:15:19 +0300

    Simple copying value

---
+++ array-deep-clone/copying_value.js
@@ -0,0 +1,11 @@
+let value = 3;
+let valueCopy = value; // create copy
+
+console.log(valueCopy); // 3
+
+// Change valueCopy
+valueCopy = 100
+console.log(valueCopy); // 100
+
+// ✅ Original NOT affected 
+console.log(value); // 3
