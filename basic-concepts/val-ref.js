04314d54b25067831d40c666d2c781d2a534249a
YaroslavW <doctor355@yahoo.com>
2019-03-18 10:44:28 +0300

    Val and Ref with Objects

--- basic-concepts/val-ref.js
+++ basic-concepts/val-ref.js
@@ -7,5 +7,13 @@ console.log(str_2); // It is string
 
 str_2 = 'My NEW string';
 
-console.log(`str-1 = ${str_1}`); // It is string
-console.log(`str-2 = ${str_2}`); // My NEW string
+console.log(`str-1 = ${str_1}`); // str-1 = It is string
+console.log(`str-2 = ${str_2}`); // str-2 = My NEW string
+
+let obj_1 = {name: 'Yaroslav'};
+let obj_2 = obj_1;
+
+obj_2.name = 'Kolesnikov';
+
+console.log(`obj_1 = ${obj_1.name}`);  // obj_1 = Kolesnikov;
+console.log(`obj_2 = ${obj_2.name}`); // obj_2 = Kolesnikov
