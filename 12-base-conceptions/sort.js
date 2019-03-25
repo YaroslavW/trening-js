b1e26531760f7edc79bfb36c3f69aef32c35c996
YaroslavW <doctor355@yahoo.com>
2019-03-25 16:09:47 +0300

    Sort arrays

---
+++ 12-base-conceptions/sort.js
@@ -0,0 +1,19 @@
+let arr = [1, 2, 10, 21];
+arr.sort(); // [1, 10, 2, 21]
+
+let arr_1 = [1, 7, 3, -1, 5, 7, 2];
+const sorter = (firstEl, secondEl) => firstEl - secondEl;
+let sorted = arr_1.sort(sorter);
+console.log(sorted);
+
+
+
+let john = { name: "John", age: 23 };
+let jane = { name: "Jane", age: 18 };
+let jerry = { name: "Jerry", age: 34 };
+
+let people = [john, jane, jerry];
+let sorter_1 = (a, b) => a.age - b.age;
+
+let sortedPeople = people.sort(sorter_1);
+console.log(sortedPeople);
