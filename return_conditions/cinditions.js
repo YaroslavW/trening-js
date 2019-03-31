5e6ba64e6be2a512477b86e704c0be0c6bd7a286
Yaroslav <doctor355@yahoo.com>
2019-03-31 14:21:34 +0300

    Return condition

---
+++ return_conditions/cinditions.js
@@ -0,0 +1,13 @@
+// function cond(a, b){
+//  if(a>b){
+//      return a
+//  }
+//  else {
+//      return b
+//  }
+// }
+
+function cond(x, y){
+    return (x>y) ? y : x
+}
+console.log(cond(5,3))
