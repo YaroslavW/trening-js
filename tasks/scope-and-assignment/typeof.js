7ddfdc3c318f8fcb651287b1f6ecfba3851ab5e3
Yaroslav <doctor355@yahoo.com>
2019-04-01 17:22:07 +0300

    Scope IIFE - Immediately Invoked Function Expression and assignment

---
+++ tasks/scope-and-assignment/typeof.js
@@ -0,0 +1,10 @@
+(function(){
+    // var a = 4;  //undefined
+    // var b = 3; //undefined
+    var a = b  =3;
+})();
+// console.log(typeof a, typeof b);  // undefined number 
+// Because 'b' falls in the global scope.
+
+console.log(b); // 3
+console.log(a); // Error
