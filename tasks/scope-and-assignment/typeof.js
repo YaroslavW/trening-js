b2479a69bfc5d0c017dba49e6ce48a405469d762
Yaroslav <doctor355@yahoo.com>
2019-04-01 17:28:40 +0300

    IIFE - types

--- tasks/scope-and-assignment/typeof.js
+++ tasks/scope-and-assignment/typeof.js
@@ -2,9 +2,21 @@
     // var a = 4;  //undefined
     // var b = 3; //undefined
     var a = b  =3;
-})();
+})(); // dog balls
 // console.log(typeof a, typeof b);  // undefined number 
 // Because 'b' falls in the global scope.
 
 console.log(b); // 3
 console.log(a); // Error
+
+
+
+(function () {
+    // код функции 
+}());
+
+
+// Dog Balls
+(function() {
+  //body 
+})();
