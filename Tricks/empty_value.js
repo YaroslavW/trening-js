1bf2bb7adc21fc588bbbf5203e4d206122a3fd4f
Yaroslav <doctor355@yahoo.com>
2019-05-22 19:25:11 +0300

    change two same var - names

--- Tricks/empty_value.js
+++ Tricks/empty_value.js
@@ -1,12 +1,12 @@
-var arr = [ 0, 1 , 2, '', ' ', ' ', '0', '1'];
+// var arr = [ 0, 1 , 2, '', ' ', ' ', '0', '1'];
 
-arr1 = arr.filter(el => el>0 || parseInt(el)===0);
-console.log(arr1);
+// arr1 = arr.filter(el => el>0 || parseInt(el)===0);
+// console.log(arr1);
 
-=======
+// =======
 const arr = [ 1, 0, '1', '2', '3', '0', '', ' ' ];
 
 arr1 = arr.filter(el => !isNaN(parseInt(el)));
 console.log(arr1);
 
-=====
+// =====
