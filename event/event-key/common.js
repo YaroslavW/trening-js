fa08a11c628174443cdd4a93f7e495cdb4c83bda
Yaroslav <doctor355@yahoo.com>
2019-06-13 20:38:01 +0300

    Keydown for hidden

---
+++ event/event-key/common.js
@@ -0,0 +1,11 @@
+let flag = false;
+const y = document.getElementById("Y");
+document.onkeydown = function(event) {
+  // console.log(event);
+  if (event.code == "AltLeft") flag = true;
+  if (event.code == "KeyY" && flag) {
+    flag = false;
+    console.log("work");
+    y.style.visibility = "hidden";
+  }
+};
