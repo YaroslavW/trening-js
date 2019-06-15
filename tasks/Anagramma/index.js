060ffbc8c6ce7172e6f1eb470e67bcfaf6928c75
Yaroslav <doctor355@yahoo.com>
2019-06-15 13:05:18 +0300

    Function with funct transform str to aarray

--- tasks/Anagramma/index.js
+++ tasks/Anagramma/index.js
@@ -1,8 +1,25 @@
-const str_1 = "С Новым Годом";
+// const str_1 = "С Новым Годом";
 
-const arr_1 = str_1
-  .toLocaleLowerCase() // нижний регистр
-  .replace(/\s/g, "") //убрать пробелы внутри
-  .split(""); //в массив
+// const arr_1 = str_1
+//   .toLocaleLowerCase() // нижний регистр
+//   .replace(/\s/g, "") //убрать пробелы внутри
+//   .split(""); //в массив
 
-console.log(arr_1);
+// console.log(arr_1);
+// // [ 'с', 'н', 'о', 'в', 'ы', 'м', 'г', 'о', 'д', 'о', 'м' ]
+
+function checkingAnagramm(userString, anagramm) {
+  function toArray(valuue) {
+    return valuue
+      .toLocaleLowerCase()
+      .replace(/\s/g, "")
+      .split("");
+  }
+  let userArray = toArray(userString);
+  let anagrammArray = toArray(anagramm);
+  return console.log(
+    "UserArra = " + userArray + " ; " + " AnagrammArray = " + anagrammArray
+  );
+}
+
+checkingAnagramm("  П Р ивет", " Hel l o ");
