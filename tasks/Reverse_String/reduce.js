9d52de62b29f1a9d6d910702af43558163238c5f
Yaroslav <doctor355@yahoo.com>
2019-07-19 10:51:39 +0300

    Task Reverse String solution-3 reduce()

---
+++ tasks/Reverse_String/reduce.js
@@ -0,0 +1,19 @@
+function reverseString(string) {
+  const arr = string.split("");
+  const reversedString = arr.reduce((reversed, character) => {
+    return character + reversed;
+  }, "");
+  return reversedString;
+}
+console.log(reverseString("Hello"));
+// olleH
+
+const reverString = string => {
+  return string.split("").reduce((rever, chart) => chart + rever, "");
+};
+console.log(reverString("JavaScript"));
+// tpircSavaJ
+
+const revString = string =>
+  string.split("").reduce((rev, char) => char + rev, "");
+console.log(revString("React"));
