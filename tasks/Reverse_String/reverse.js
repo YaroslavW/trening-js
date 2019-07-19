22e1b384c88197c051781c6efac7b053a1078a8a
Yaroslav <doctor355@yahoo.com>
2019-07-19 10:15:11 +0300

    Task Reverse String solution-1 reverse()

---
+++ tasks/Reverse_String/reverse.js
@@ -0,0 +1,15 @@
+function reverseString(string) {
+  let arr = string.split("");
+  arr.reverse();
+  return arr.join("");
+}
+console.log(reverseString("Hello"));
+// olleH
+
+// As arrow function
+const revStr = string_1 =>
+  string_1
+    .split("")
+    .reverse()
+    .join("");
+console.log(revStr("JavaScript"));
