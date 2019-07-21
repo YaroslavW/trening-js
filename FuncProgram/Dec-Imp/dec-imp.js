aa21f8bb96cadc43fee0fb971f8ecc9f7b41c709
Yaroslav <doctor355@yahoo.com>
2019-07-21 17:30:22 +0300

    add examples

--- FuncProgram/Dec-Imp/dec-imp.js
+++ FuncProgram/Dec-Imp/dec-imp.js
@@ -4,3 +4,15 @@ function ReactComponent({ counter }) {
 }
 
 // Императивный код описывает, как он это делает:
+function UpdateCounter({ counter }) {
+  document.getElementById("counter").innerHTML(`<span>${counter}<span>`);
+}
+// ex
+function getFileMapById(files) {
+  const fileMap = {};
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    fileMap[file.id] = file;
+  }
+  return fileMap;
+}
