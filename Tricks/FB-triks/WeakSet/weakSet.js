5de57428247d2a9b5e1e2c62e5b0b22eba4605b1
Yaroslav <doctor355@yahoo.com>
2019-07-30 13:55:13 +0300

    WeakSet with obj

--- Tricks/FB-triks/WeakSet/weakSet.js
+++ Tricks/FB-triks/WeakSet/weakSet.js
@@ -1,5 +1,12 @@
 const ws = new WeakSet();
-const one = {};
+const one = 1;
 ws.add(one);
 const hasOne = ws.has(one);
 console.log(hasOne);
+// Error
+// const ws_1 = new WeakSet();
+// const one_1 = {};
+// ws_1.add(one_1);
+// const hasOne_1 = ws_1.has(one_1);
+// console.log(hasOne_1);
+// true
