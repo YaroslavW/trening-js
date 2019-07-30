b81dc719beb5a273ef3d589f237f989839fbe918
Yaroslav <doctor355@yahoo.com>
2019-07-30 13:56:11 +0300

    add pic

--- Tricks/FB-triks/WeakSet/weakSet.js
+++ Tricks/FB-triks/WeakSet/weakSet.js
@@ -4,9 +4,9 @@ ws.add(one);
 const hasOne = ws.has(one);
 console.log(hasOne);
 // Error
-// const ws_1 = new WeakSet();
-// const one_1 = {};
-// ws_1.add(one_1);
-// const hasOne_1 = ws_1.has(one_1);
-// console.log(hasOne_1);
+const ws_1 = new WeakSet();
+const one_1 = {};
+ws_1.add(one_1);
+const hasOne_1 = ws_1.has(one_1);
+console.log(hasOne_1);
 // true
