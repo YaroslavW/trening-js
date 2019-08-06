349e2f043e98cbd333d4036e10ae9efa7727f82d
Yaroslav <doctor355@yahoo.com>
2019-08-06 12:00:00 +0300

    FB-task obj-props  as string and number with breackets notation

---
+++ FB-tasks/sum_objs_prop/sum_obj.js
@@ -0,0 +1,8 @@
+// https://www.facebook.com/levelhunt/photos/a.1422238711243405/1666694233464517/?type=3&theater&ifg=1
+
+const object = { "0": 1, 0: 2 };
+console.log(`Obect with "" =  ${object["0"]}`);
+console.log(`Obect without "" =  ${object[0]}`);
+const result = object["0"] + object[0];
+console.log(result);
+// 4
