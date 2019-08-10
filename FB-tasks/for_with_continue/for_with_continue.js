d78a08c3e8d74bd9fd559ac71bf6f66cda1cc3c4
Yaroslav <doctor355@yahoo.com>
2019-08-10 12:00:00 +0300

    first simple answer

--- FB-tasks/for_with_continue/for_with_continue.js
+++ FB-tasks/for_with_continue/for_with_continue.js
@@ -1,1 +1,10 @@
 // https://www.facebook.com/levelhunt/photos/a.1422238711243405/1667370123396928/?type=3&theater&ifg=1
+let x = 0;
+for (let i = 0; i <= 6; i += 2) {
+  if (i === 4) {
+    continue;
+  }
+  x += i;
+}
+console.log(x);
+// 8
