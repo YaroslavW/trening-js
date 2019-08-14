edf275e3a84057ef9dcf6ea82ccac7e08c878a91
Yaroslav <doctor355@yahoo.com>
2019-08-14 11:19:17 +0300

    added example

--- NewTasks/FB/Banana-NaN-trick/banana.js
+++ NewTasks/FB/Banana-NaN-trick/banana.js
@@ -1,6 +1,11 @@
-console.log(("b" + "a" + +"a" + "a").toLocaleLowerCase());
-
+console.log(("b" + "a" + +"a" + "a").toLowerCase());
+// banana;
+//
 console.log("b" + "a" + +"a" + "a");
 // baNaNa
 // Второе + 'a' рассматривается, как приведение к числу - дает NaN и далее добавляется последнее 'a'
 // Просто приведение к нижнему регистру немного путает
+
+// sonans
+console.log(("s" + "o" + +"x" + "s").toLowerCase());
+// sonans
