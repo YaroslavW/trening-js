f454b6060935275f51536f1c9a6cb9c966193250
Yaroslav <doctor355@yahoo.com>
2019-08-26 13:31:03 +0300

    get rid duplicates with newSet on String-arr - case-sensetive

--- Tricks/FB-triks/Duplicates/get_rid_of_duplicates.js
+++ Tricks/FB-triks/Duplicates/get_rid_of_duplicates.js
@@ -4,3 +4,10 @@ const arr_1 = [1, 1, 2, 3, 2, 4, 2, 4, 3, 5, 3, 1, 3];
 
 const arr1 = unique_arr(arr_1);
 console.log(arr1);
+// [ 1, 2, 3, 4, 5 ]
+
+const arr_2 = ["C#", "JS", "js", "c#", "C#", "#JS", "C#", "#js"];
+const arr2 = unique_arr(arr_2);
+console.log(arr2);
+// [ 'C#', 'JS', 'js', 'c#', '#JS', '#js' ]
+// NOTE: Be careful strings are case-sensitive..
