0d6005c529f423577b13ac014a3a0724f4c6ff6c
Yaroslav <doctor355@yahoo.com>
2019-08-27 22:19:57 +0300

    old method entries

--- ES-new/ES2017/fromEntries/fromEntries.js
+++ ES-new/ES2017/fromEntries/fromEntries.js
@@ -16,3 +16,26 @@ Object.entries(students);
 //  [ 'deirdre', 19 ],
 //  [ 'eloise', 21 ]
 // ]
+
+// Но для того, чтобы преобразовать массив обратно в объект,
+// к сожалению, удобных средств не существовало.
+//  Всё приходилось делать вручную, с помощью цикла:
+let students = {
+  amelia: 20,
+  beatrice: 22,
+  cece: 20,
+  deirdre: 19,
+  eloise: 21
+};
+
+// преобразуем объект в массив для того чтобы воспользоваться методом .filter()
+let overTwentyOne = Object.entries(students).filter(([name, age]) => {
+  return age >= 21;
+}); // [ [ 'beatrice', 22 ], [ 'eloise', 21 ] ]
+
+// преобразуем многомерный массив обратно в объект
+let drinkingAgeStudents = {};
+for (let [name, age] of overTwentyOne) {
+  drinkingAgeStudents[name] = age;
+}
+// { beatrice: 22, eloise: 21 }
