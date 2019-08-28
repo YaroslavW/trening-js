7279d600de0f1d3472a99ac38bb982dc495fd2cb
Yaroslav <doctor355@yahoo.com>
2019-08-28 10:10:58 +0300

    operator delete

---
+++ Tricks/FB-triks/Delete/delete.js
@@ -0,0 +1,10 @@
+const abverb = "Just";
+name = "Java Script";
+
+const abverb_result = delete abverb;
+const name_result = delete name;
+
+console.log(abverb_result);
+// false
+console.log(name_result);
+//  true
