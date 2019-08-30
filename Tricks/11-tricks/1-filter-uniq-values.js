458c897c8a089c107cdb580fb4edbad2600a94a2
Yaroslav <doctor355@yahoo.com>
2019-08-30 19:17:19 +0300

    Notes added

--- Tricks/11-tricks/1-filter-uniq-values.js
+++ Tricks/11-tricks/1-filter-uniq-values.js
@@ -1,3 +1,7 @@
 const array = [1, 1, 2, 3, 5, 5, 1];
 const uniqueArray = [...new Set(array)];
 console.log(uniqueArray);
+// [ 1, 2, 3, 5 ]
+// This trick works for arrays containing primitive types: undefined, null, boolean,
+//  string and number . (If you had an array containing objects, functions or additional
+//  arrays, you’d need a different approach!)
