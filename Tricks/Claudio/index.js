288d0e4ebca8861979a814bcb3998ead59d57f45
Yaroslav <doctor355@yahoo.com>
2019-08-31 18:23:17 +0300

    simple exaple too simple ex

---
+++ Tricks/Claudio/index.js
@@ -0,0 +1,26 @@
+// https://www.facebook.com/groups/1517091958315927/permalink/3259539914071114/
+//Declaro la función para aceptar tres valores
+
+function perimetroTriangulo(l1, l2, l3) {
+  //calculo el perimetro lado 11+ lado2+ lado3
+
+  let perimetro = l1 + l2 + l3;
+
+  //imprimo el valor resultante en la pantalla
+
+  console.log("El perimetro es de " + perimetro);
+}
+
+//Declaro mis varibles de lado1, lado2, lado3
+
+const l1 = 10;
+
+const l2 = 10;
+
+const l3 = 10;
+
+//llamo a la funcion con estos valores
+
+// perimetroTriangulo = l1 + l2 + l3;
+perimetroTriangulo(l1, l2, l3);
+// El perimetro es de 30
