dee8dae0b8dcb98b7030ff68dbba2ae2df5af7c9
Yaroslav <doctor355@yahoo.com>
2019-09-04 19:20:39 +0300

    trimRight-left as old methods

---
+++ es6/2019/trimStart_End/old.js
@@ -0,0 +1,7 @@
+let message = "   Welcome to CS 101    ";
+message.trimRight();
+// '   Welcome to CS 101'
+message.trimLeft();
+// 'Welcome to CS 101   '
+message.trimRight().trimLeft();
+// 'Welcome to CS 101'
