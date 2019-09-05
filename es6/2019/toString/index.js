d84b62167773900f7061fcaa8e7459ea68479f50
Yaroslav <doctor355@yahoo.com>
2019-09-05 20:16:13 +0300

    toString new

--- es6/2019/toString/index.js
+++ es6/2019/toString/index.js
@@ -7,3 +7,15 @@ function greeting() {
 
 greeting.toString();
 //'function greeting() {\nconst name = \'CSS Tricks\'\nconsole.log(`hello from ${name} //`)\n}
+
+// Теперь этот метод отражает реальное представление исходного кода функций.
+function greeting() {
+  const name = "CSS Tricks";
+  console.log(`hello from ${name}`);
+}
+
+greeting.toString();
+// 'function greeting() {\n' +
+//  "  const name = 'CSS Tricks'\n" +
+//  '  console.log(`hello from ${name}`)\n' +
+//  '}'
