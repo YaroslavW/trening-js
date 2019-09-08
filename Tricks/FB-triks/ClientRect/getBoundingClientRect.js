acab46af6dfb9db636a6984ca50ab98675a620f6
Yaroslav <doctor355@yahoo.com>
2019-09-08 16:25:32 +0300

    getBoundingClientRect

---
+++ Tricks/FB-triks/ClientRect/getBoundingClientRect.js
@@ -0,0 +1,11 @@
+const isInViewport = function(elem) {
+  const distance = elem.getBoundingClientRect();
+  return (
+    distance.top >= 0 &&
+    distance.left >= 0 &&
+    distance.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    distance.right <=
+      (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
