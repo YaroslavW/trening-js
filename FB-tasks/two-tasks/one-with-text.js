6efa77be1001c04e901bc555c9c6a7f60c792fbe
Yaroslav <doctor355@yahoo.com>
2019-09-10 23:18:55 +0300

    FB-task separate Vowes - first part split

---
+++ FB-tasks/two-tasks/one-with-text.js
@@ -0,0 +1,15 @@
+// Hey guys could someone help me on this using JavaScript
+// Title:None,Content:Write a function that takes a string and returns an array of length 2
+//  containing a new string made of all and only the vowels from the original string and the
+// number of duplicates in the original string
+
+// Write a function named fizzbuzz that takes two 2 arrays and returns Fizz if the combined
+// length of the lists is divisible by 3, Buzz if it is divisible by 5, fizzbuzz if it is
+// divisible by both 5 and 3
+
+// ======================>>> 1 <<<==========================
+function separateVowels(str) {
+  const str_to_arr = str.split("");
+  console.log(str_to_arr);
+}
+separateVowels("Hello");
