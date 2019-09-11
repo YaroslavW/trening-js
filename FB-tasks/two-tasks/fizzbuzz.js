11d610958d4bb83f3c049b795117bab7c8500a7c
Yaroslav <doctor355@yahoo.com>
2019-09-11 12:35:38 +0300

    arr length and other arrays

---
+++ FB-tasks/two-tasks/fizzbuzz.js
@@ -0,0 +1,25 @@
+// Напишите функцию с именем fizzbuzz, которая принимает два 2 массива и возвращает Fizz,
+//  если объединенный
+// длина списков делится на 3, Buzz, если он делится на 5, fizzbuzz, если это
+// делится на 5 и 3
+
+function fizzbuzz(arr_1, arr_2) {
+  let com_arr = [...arr_1, ...arr_2];
+  console.log(com_arr);
+  let Fizz = [];
+  let Buzz = [];
+  let fizzbuzz = [];
+  if (com_arr.length % 3 === 0) {
+    Fizz = [...com_arr];
+    console.log(`Fizz = ${Fizz}`);
+    if (com_arr.length % 5 === 0) {
+      Buzz = [...com_arr];
+      console.log(`Buzz = ${Buzz}`);
+    }
+  } else {
+    fizzbuzz = [...com_arr];
+    console.log(`fizzbuzz = ${fizzbuzz}`);
+  }
+}
+
+fizzbuzz([1, 2], [5]);
