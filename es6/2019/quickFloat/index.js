46059013674631e062c922153bdfab2462a5e850
Yaroslav <doctor355@yahoo.com>
2019-09-12 19:03:33 +0300

    Remove Final Digits

--- es6/2019/quickFloat/index.js
+++ es6/2019/quickFloat/index.js
@@ -5,3 +5,17 @@
 
 console.log(23.9 | 0); // Result: 23
 console.log(-23.9 | 0); // Result: -23
+
+//==================>>> Remove Final Digits <<<=====================
+
+// The bitwise OR operator can also be used to remove any amount
+// of digits from the end of an integer.
+//  This means we don’t have to use code like this to convert between types:
+
+let str = "1553";
+Number(str.substring(0, str.length - 1));
+// Instead, the bitwise OR operator allows us to write:
+
+console.log((1553 / 10) | 0); // Result: 155
+console.log((1553 / 100) | 0); // Result: 15
+console.log((1553 / 1000) | 0); // Result: 1
