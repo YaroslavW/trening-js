1421c9e8a66fd8183e885fd7f287630ec0eaa0d1
Yaroslav <doctor355@yahoo.com>
2019-09-12 18:59:35 +0300

    added notes

--- es6/2019/quickPower/index.js
+++ es6/2019/quickPower/index.js
@@ -1,1 +1,12 @@
 console.log(2 ** 3); // Result: 8
+
+// This shouldn’t be confused with the ^ symbol, commonly used to represent exponents, but which in JavaScript is the bitwise XOR operator.
+
+// Before ES7, shorthand existed only for powers with base 2,
+// using the bitwise left shift operator << :
+
+// The following expressions are equivalent:
+// Math.pow(2, n);
+// 2 << (n - 1);
+// 2**n;
+// For example, 2 << 3 = 16 is equivalent to 2 ** 4 = 16 .
