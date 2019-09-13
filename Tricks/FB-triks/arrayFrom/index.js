53574f2d0696a7d8835849c6e2f37a67b2ae7a82
Yaroslav <doctor355@yahoo.com>
2019-09-13 12:00:19 +0300

    Array from old method

---
+++ Tricks/FB-triks/arrayFrom/index.js
@@ -0,0 +1,8 @@
+// Fill an array with a random length (between 0 and 499)
+const randomAmountOfArrayItems = Math.floor(Math.random() * 500);
+
+// Old way
+var arr_1 = [];
+for (var i = 0; i < randomAmountOfArrayItems; i++) {
+  arr_1.push("");
+}
