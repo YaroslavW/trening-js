54ce44600db9a5d168e7bdcb9cc58422ecda865d
Yaroslav <doctor355@yahoo.com>
2019-09-14 09:17:07 +0300

    FB-tricks with let

--- Tricks/FB-triks/BuildFunc/index.js
+++ Tricks/FB-triks/BuildFunc/index.js
@@ -12,3 +12,18 @@ var fs = buildFunctions();
 fs[0](); // 3
 fs[1](); // 3
 fs[2](); // 3
+// ===================================>>> With let <<<=======================
+function buildFunctions_1() {
+  var arr_1 = [];
+  for (let i = 0; i < 3; i++) {
+    arr_1.push(function() {
+      console.log(i);
+    });
+  }
+  return arr_1;
+}
+
+var fs = buildFunctions_1();
+fs[0](); // 0
+fs[1](); // 1
+fs[2](); // 2
