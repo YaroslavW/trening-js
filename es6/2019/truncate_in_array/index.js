1082f7f511f8929dc0e3a7286db7d7efffcb6d9f
Yaroslav <doctor355@yahoo.com>
2019-09-17 23:20:46 +0300

    Truncate an Array part -2

--- es6/2019/truncate_in_array/index.js
+++ es6/2019/truncate_in_array/index.js
@@ -7,3 +7,10 @@
 let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 array.length = 4;
 console.log(array); // Result: [0, 1, 2, 3]
+
+// This is a particularly concise solution. However, I have found the run-time of the slice()
+//  method to be even faster. If speed is your main goal, consider using something like this:
+
+let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+array = array.slice(0, 4);
+console.log(array); // Result: [0, 1, 2, 3]
