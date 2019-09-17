b08a3b597e3f790504fe8efa3adbdba9e84fd9f4
Yaroslav <doctor355@yahoo.com>
2019-09-17 23:27:44 +0300

    added notes and comments

--- es6/2019/depr_cach_arr_length/index.js
+++ es6/2019/depr_cach_arr_length/index.js
@@ -1,12 +1,13 @@
 // In the original version of this article, I shared a tip to cache array length in for loops.
-//  However, if it is a read-only loop, modern JavaScript engines deal with this at the point of 
+//  However, if it is a read-only loop, modern JavaScript engines deal with this at the point of
 //  compilation. It is no longer necessary unless the length of the array changes (and, if that is the case, you’ll probably want it to be recalculated with every iteration anyway).
-
 
 // Thanks to several commenters who pointed this out. If you’d like to find more, check out this
 //  question on StackOverflow.
 
-// For those who are interested, there used to be some performance incentive to writing 
+// For those who are interested, there used to be some performance incentive to writing
 
-for (let i = 0, len = array.length; i < len; i++) rather than for (let i = 0; i < array.length; i++)  
+// for (let i = 0, len = array.length; i < len; i++)
+// rather than for
+// (let i = 0; i < array.length; i++)
 // This is no longer the case!
