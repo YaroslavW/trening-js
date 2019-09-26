9e766357b486657319845dd38f497e5f9ef5d979
Yaroslav <doctor355@yahoo.com>
2019-09-26 13:15:51 +0300

    forEach trick - unfortunated

--- array/forEach/index.js
+++ array/forEach/index.js
@@ -6,3 +6,12 @@ arr.forEach(function(item) {
   return arr_1.push(item * 20);
 });
 console.log(arr_1);
+
+function s(arr) {
+  sum = 0;
+  arr.forEach(function(item, sum) {
+    return console.log((sum += item));
+  });
+}
+
+s(arr);
