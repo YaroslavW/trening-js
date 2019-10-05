01f0dfb8cbbf71c7e1d7ec7545c02f5e6cee1a10
Yaroslav <doctor355@yahoo.com>
2019-10-05 17:14:16 +0300

    RE - find and change num in str

---
+++ Tricks/RegExp/only_abc/index.js
@@ -0,0 +1,10 @@
+function replNumbers(str) {
+  return str.replace(/\d+/g, "digital");
+}
+
+console.log(replNumbers("Alina cabaeva .65743bbb902"));
+// Alina cabaeva .digitalbbbdigital;
+// replNumbers()
+// replNumbers()
+// replNumbers()
+// replNumbers()
