3125bfd1f2485cbdea8a40d2fe155e348dd41a80
Yaroslav <doctor355@yahoo.com>
2019-10-06 11:34:02 +0300

    FB-trick Diff between two arr - added notes and comments

--- Tricks/FB-triks/diff_two_arr/index.js
+++ Tricks/FB-triks/diff_two_arr/index.js
@@ -1,26 +1,27 @@
 const arr_1 = [5, 2, 1, 8];
 const arr_2 = [1, 2, 3, 4, 5];
+//  ====>> Filter + includes
+const diff_arr = [
+  ...arr_2.filter(x => !arr_1.includes(x)),
+  ...arr_1.filter(x => !arr_2.includes(x))
+];
 
-// const diff_arr = [
-//   ...arr_2.filter(x => !arr_1.includes(x)),
-//   ...arr_1.filter(x => !arr_2.includes(x))
-// ];
-
-// console.log(diff_arr);
+console.log(diff_arr);
 
 // [3, 4, 8]; unicum num;
 
-// As functiom
-// function arr_diff(a, b) {
-//   return [
-//     ...a.filter(x => b.indexOf(x) === -1),
-//     ...b.filter(x => a.indexOf(x) === -1)
-//   ];
-// }
+//=====>> As functiom with indexOf + filter
+function arr_diff(a, b) {
+  return [
+    ...a.filter(x => b.indexOf(x) === -1),
+    ...b.filter(x => a.indexOf(x) === -1)
+  ];
+}
 
-// console.log(arr_diff(arr_1, arr_2));
+console.log(arr_diff(arr_1, arr_2));
 // [ 8, 3, 4 ]
 
+// ====>> With SET + filter
 const diff = (a, b) => {
   const setA = new Set(a);
   const setB = new Set(b);
