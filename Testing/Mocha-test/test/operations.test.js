16eab5b1023c6d22a1ec8a16886805515403aafd
Yaroslav <doctor355@yahoo.com>
2020-04-08 19:02:34 +0300

    test Mocha without errors

--- Testing/Mocha-test/test/operations.test.js
+++ Testing/Mocha-test/test/operations.test.js
@@ -44,7 +44,7 @@
 var operations = require("../operations");
 
 it("should multiply two numbers", function () {
-  var expectedResult = 16;
+  var expectedResult = 15;
   var result = operations.multiply(3, 5);
   if (result !== expectedResult) {
     throw new Error(`Expected ${expectedResult}, but got ${result}`);
