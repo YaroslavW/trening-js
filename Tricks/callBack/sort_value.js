2c5b0e94510650d9d0097da96c6cb773edf76036
Yaroslav <doctor355@yahoo.com>
2020-04-15 09:16:00 +0300

    rev sort with callback as value

--- Tricks/callBack/sort_value.js
+++ Tricks/callBack/sort_value.js
@@ -13,18 +13,28 @@ var simpleArray = [
   },
 ];
 
-simpleArray.sort(function (val_1, val_2) {
-  if (val_1.value < val_2.value) {
-    return -1;
-  } else {
-    return 1;
-  }
-});
+// simpleArray.sort(function (val_1, val_2) {
+//   if (val_1.value < val_2.value) {
+//     return -1;
+//   } else {
+//     return 1;
+//   }
+// });
 
 // 12 Hello
 // 14 Something
 // 13 World
 
+simpleArray.sort(function (val_1, val_2) {
+  if (val_1.value > val_2.value) {
+    return -1;
+  } else {
+    return 1;
+  }
+});
+// 13 World
+// 14 Something
+// 12 Hello
 let result = simpleArray.forEach((res) =>
   console.log(res.key + " " + res.value)
 );
