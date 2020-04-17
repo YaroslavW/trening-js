b2ae6b5dffab636700094bf07607eeab9e83191b
Yaroslav <doctor355@yahoo.com>
2020-04-17 18:24:00 +0300

    trick-2 with add_str

--- FB-tasks/add_str/es6.js
+++ FB-tasks/add_str/es6.js
@@ -1,13 +1,20 @@
-let add_str = (str) => {
-  str += "-";
-  str.length < 26 ? add_str(str) : str;
-  console.log(str);
-};
+// let add_str = (str) => {
+//   str += "-";
+//   str.length < 26 ? add_str(str) : str;
+//   console.log(str);
+// };
 
-add_str("JavaScript Evrywhere");
+// add_str("JavaScript Evrywhere");
 // JavaScript Evrywhere------
 // JavaScript Evrywhere-----
 // JavaScript Evrywhere----
 // JavaScript Evrywhere---
 // JavaScript Evrywhere--
 // JavaScript Evrywhere-
+
+let add_str = (str) => {
+  str += "-";
+  str.length < 26 ? add_str(str) : console.log(str);
+};
+add_str("JavaScript Evrywhere");
+// JavaScript Evrywhere------
