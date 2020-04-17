e25d7d866298268b959ca78d322260295842caa7
Yaroslav <doctor355@yahoo.com>
2020-04-17 18:42:00 +0300

    recursion for one add_str

--- FB-tasks/add_str/es6.js
+++ FB-tasks/add_str/es6.js
@@ -12,9 +12,9 @@
 // JavaScript Evrywhere--
 // JavaScript Evrywhere-
 
-let add_str = (str) => {
-  str += "-";
-  str.length < 26 ? add_str(str) : console.log(str);
+let add_str = (str) => {  
+  str.length < 26 ? add_str(str + '-') : console.log(str);
 };
 add_str("JavaScript Evrywhere");
 // JavaScript Evrywhere------
+
