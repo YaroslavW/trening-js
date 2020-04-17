52b1f6c22fe346746eb7f8144cea65ac9dfa37f7
Yaroslav <doctor355@yahoo.com>
2020-04-17 18:47:00 +0300

    second separate recursion for add_str fill

--- FB-tasks/add_str/es6.js
+++ FB-tasks/add_str/es6.js
@@ -12,9 +12,14 @@
 // JavaScript Evrywhere--
 // JavaScript Evrywhere-
 
-let add_str = (str) => {  
-  str.length < 26 ? add_str(str + '-') : console.log(str);
+let add_str = (str) => {
+  str.length < 26 ? add_str(str + "-") : console.log(str);
 };
 add_str("JavaScript Evrywhere");
 // JavaScript Evrywhere------
 
+let add_str_2 = (str) => {
+  str.length < 26 ? add_str_2("~" + str) : console.log(str);
+};
+add_str_2("JavaScript Evrywhere");
+// ~~~~~~JavaScript Evrywhere
