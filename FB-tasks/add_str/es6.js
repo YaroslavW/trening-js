b3a3f86be666cf1d52160af51e359485cb3f54c8
Yaroslav <doctor355@yahoo.com>
2020-04-18 08:50:00 +0300

    recursion as two func add_str

--- FB-tasks/add_str/es6.js
+++ FB-tasks/add_str/es6.js
@@ -15,11 +15,20 @@
 let add_str = (str) => {
   str.length < 26 ? add_str(str + "-") : console.log(str);
 };
-add_str("JavaScript Evrywhere");
+// add_str("JavaScript Evrywhere");
 // JavaScript Evrywhere------
 
 let add_str_2 = (str) => {
   str.length < 26 ? add_str_2("~" + str) : console.log(str);
 };
-add_str_2("JavaScript Evrywhere");
+// add_str_2("JavaScript Evrywhere");
+// ~~~~~~JavaScript Evrywhere
+
+let add_str_common = (str) => {
+  add_str(str);
+  add_str_2(str);
+};
+
+add_str_common("JavaScript Evrywhere");
+// JavaScript Evrywhere------
 // ~~~~~~JavaScript Evrywhere
