facfe05e363a7c8fd676552300d6b43594fc36c7
Yaroslav <doctor355@yahoo.com>
2020-04-19 09:23:00 +0300

    with while-loop

---
+++ FB-tasks/For_loop_step/while.js
@@ -0,0 +1,29 @@
+function numberIncrease(n) {
+  var i = 5;
+  while (n > 0) {
+    console.log(i);
+    i += 4;
+    n--;
+  }
+}
+// numberIncrease(5);
+// 5;
+// 9;
+// 13;
+// 17;
+// 21;
+
+// numberIncrease(9);
+// 5;
+// 9;
+// 13;
+// 17;
+// 21;
+// 25;
+// 29;
+// 33;
+// 37;
+
+numberIncrease(2);
+// 5;
+// 9;
