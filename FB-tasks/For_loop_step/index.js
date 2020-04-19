1f6a4bccba8074eeb84e5557eda79874ee2985c1
Yaroslav <doctor355@yahoo.com>
2020-04-19 08:24:00 +0300

    simple for-loop

--- FB-tasks/For_loop_step/index.js
+++ FB-tasks/For_loop_step/index.js
@@ -4,3 +4,26 @@
 // input => numberIncrease(9)
 // output => 5, 9, 13, 17, 21. 25, 29, 33, 37
 
+function numberIncrease(n) {
+  for (i = 5; i <= 4 * n + 1; i += 4) {
+    console.log(i);
+  }
+}
+
+// numberIncrease(5);
+// 5;
+// 9;
+// 13;
+// 17;
+// 21;
+// numberIncrease(9);
+// 5;
+// 9;
+// 13;
+// 17;
+// 21;
+// 25;
+// 29;
+// 33;
+// 37;
+numberIncrease(2);
