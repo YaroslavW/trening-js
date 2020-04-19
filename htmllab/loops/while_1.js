662b8d23b250856bef89e9d6881090a2feb65fce
Yaroslav <doctor355@yahoo.com>
2020-04-19 10:31:00 +0300

    htmlab - while-loop - 1

---
+++ htmllab/loops/while_1.js
@@ -0,0 +1,11 @@
+// Напишите скрипт, который используя оператор while 
+// выведет все числа от 45 до 67
+
+function getNumbers(){
+  var n = 45;
+  while(n <= 67){
+    console.log(n);
+    n++;
+  }
+}
+getNumbers();
