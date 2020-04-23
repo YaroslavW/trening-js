4baa49e62ca531fead7c0366481adee3588aca06
Yaroslav <doctor355@yahoo.com>
2020-04-23 08:25:00 +0300

    setTimeout stack

---
+++ FB-tasks/setTimeout/index.js
@@ -0,0 +1,14 @@
+// Condition
+var a = 10, b = 20;
+
+setTimeout(function(){
+  console.log("JavaScript");
+  a++;
+  b++;
+  console.log(a+b);
+});
+console.log(a + b);
+
+// 30;
+// JavaScript;
+// 32;
