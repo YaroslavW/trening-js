63681908f398446d6731f3aa04d8eeebce78fbd3
Yaroslav <doctor355@yahoo.com>
2020-04-23 09:33:00 +0300

    map and parseInt fixed errors

--- FB-tasks/map_parseInt/index_1.js
+++ FB-tasks/map_parseInt/index_1.js
@@ -1,7 +1,7 @@
-// const a = ["10", "20", "30"];
-// const result = a.map(parseInt);
-// console.log(result);
-// [ 10, NaN, NaN ]
+const a = ["10", "20", "30"];
+const result = a.map(parseInt);
+console.log(result);
+[10, NaN, NaN];
 
 console.log(parseInt("30", 2));
 // NaN
