c9353f14010be63ed9e1ef2c69f64ab46ed49f30
Yaroslav <doctor355@yahoo.com>
2020-04-27 08:41:00 +0300

    define case - tasks js

---
+++ tasks/define_case/index.js
@@ -0,0 +1,7 @@
+// Напишите функцию isUpperCase(str, character), которая определяет в каком регистре написан символ строки в указанной позиции. Если в верхнем регистре выводится – true, если в нижнем – false.
+
+function isUpperCase(str, index) {
+    return str.charAt(index).toUpperCase() === str.charAt(index);
+}
+
+console.log(isUpperCase('tasks JavaScript', 6)); // true
