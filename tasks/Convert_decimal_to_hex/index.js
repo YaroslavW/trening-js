14bd2fd7d98931d9f14d990e30d80f7d7e1f8318
Yaroslav <doctor355@yahoo.com>
2020-05-12 21:03:54 +0300

    Convert decimal to hex

---
+++ tasks/Convert_decimal_to_hex/index.js
@@ -0,0 +1,9 @@
+// Напишите функцию decimalToHexString(number), которая преобразовывает десятичные значения в их шестнадцатеричный эквивалент hex. Hex (hexadecimal) — обозначение шестнадцатеричной системы счисления.
+
+function decimalToHexString(d) {
+
+return hexString = Number(d).toString(16);
+
+}
+
+console.log(decimalToHexString('100')); // 64
