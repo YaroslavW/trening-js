873ccc8ad5b59f87f6215c93dd99dbe757a2471a
Yaroslav <doctor355@yahoo.com>
2020-05-13 17:54:01 +0300

    setTimeout - two with two log

--- tasks/setTimeout_two/index.js
+++ tasks/setTimeout_two/index.js
@@ -3,8 +3,12 @@ let a = 10;
 setTimeout(() => {
   setTimeout(() => {
     a--;
+    console.log(`${a} = inner Timeout`);
+    // 9 = second Timeout
   }, 1000);
   --a;
+  console.log(`${a} = second Timeout`);
+  // 8 = inner Timeout
 }, 3000);
 
 console.log(a);
