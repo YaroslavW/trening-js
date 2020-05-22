4eeff5fcfea755bc6ebb876b629accea1109fb69
Yaroslav <doctor355@yahoo.com>
2020-05-22 14:31:33 +0300

    event loop with var

--- tasks/Testing/event_loop.js
+++ tasks/Testing/event_loop.js
@@ -1,12 +1,24 @@
 const row = (num) => {
-  for (let i = 0; i < num; i += 1) {
+  // for (let i = 0; i < num; i += 1) {
+  //   setTimeout(() => console.log(i), 0);
+  // }
+  for (var i = 0; i < num; i += 1) {
     setTimeout(() => console.log(i), 0);
   }
 };
 
 row(5);
+// with let
 // 0;
 // 1;
 // 2;
 // 3;
 // 4;
+
+
+// with var
+// 5;
+// 5;
+// 5;
+// 5;
+// 5;
