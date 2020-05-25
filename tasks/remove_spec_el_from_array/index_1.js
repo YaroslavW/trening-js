e74827759645039f0400175234864a92c9b082cc
Yaroslav <doctor355@yahoo.com>
2020-05-25 08:55:20 +0300

    Remove spec el from array ES6

--- tasks/remove_spec_el_from_array/index_1.js
+++ tasks/remove_spec_el_from_array/index_1.js
@@ -9,3 +9,10 @@ fullArray = fullArray.filter(function (item) {
 
 console.log(fullArray);
 // [1, 2, 3, 4, 5, 7, 8, 9, 10];
+
+// =========>>> ES6 <<<==========
+var valRemove = 3;
+
+fullArray = fullArray.filter((item) => item !== valRemove);
+console.log(fullArray);
+// [1, 2, 4, 5, 7, 8, 9, 10];
