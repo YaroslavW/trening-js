e453f160fe93c625151f1a64ecf877b67c2d3cb7
Yaroslav <doctor355@yahoo.com>
2020-05-26 09:34:00 +0300

    Remove all falsy values from an array-!!expression

--- tasks/Remove_false_from_array/index.js
+++ tasks/Remove_false_from_array/index.js
@@ -1,6 +1,11 @@
 let arr = [0, 1, "", undefined, false, 2, undefined, null, , 3, NaN];
 
-// Boolean constructor
+// =========>>> Boolean constructor <<<==========
 const arr_1 = arr.filter(Boolean);
 console.log(arr_1);
+// [1, 2, 3]
+
+// =========>>> !! Expression <<<==============
+const arr_2 = arr.filter((x) => !!x);
+console.log(arr_2);
 // [1, 2, 3]
