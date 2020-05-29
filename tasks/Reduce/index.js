d8b490ae575b2c5c6099fd7c93d1c87a852ae635
Yaroslav <doctor355@yahoo.com>
2020-05-29 11:19:16 +0300

    reduce

---
+++ tasks/Reduce/index.js
@@ -0,0 +1,7 @@
+let letters = ["r", "e", "d", "u", "c", "e"];
+// let word = letters.reduce(function (a, b) {
+//   return a + b;
+// }, "");
+let word = letters.reduce((a, b) => a + b, "");
+console.log(word);
+// reduce
