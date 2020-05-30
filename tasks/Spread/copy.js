90ad2ff6688947ab61953ea94848a7afa32c660f
Yaroslav <doctor355@yahoo.com>
2020-05-30 08:54:27 +0300

    Spread operator for copy and adding to array

--- tasks/Spread/copy.js
+++ tasks/Spread/copy.js
@@ -11,3 +11,23 @@ console.log(testArray);
 console.log(arrayTwo);
 // [1, 2, 3, 4, 5]
 // [1, 2, 3, 4, 5, 6, 7];
+
+const testJs = [
+  "Javascript",
+  "Developer",
+  "jsgrip",
+  "js developer",
+  "hello all",
+];
+const extraElementJs = [...testJs, "Javascript everywhere"];
+console.log(testJs);
+console.log(extraElementJs);
+// ["Javascript", "Developer", "jsgrip", "js developer", "hello all"]
+// [
+//   "Javascript",
+//   "Developer",
+//   "jsgrip",
+//   "js developer",
+//   "hello all",
+//   "Javascript everywhere"
+// ];
