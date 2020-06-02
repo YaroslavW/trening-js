954a937dc5f86663471cf4e914828d5a8a7c5678
Yaroslav <doctor355@yahoo.com>
2020-06-02 10:54:24 +0300

    comparison with new

--- tasks/with_new/index.js
+++ tasks/with_new/index.js
@@ -1,6 +1,15 @@
-const str = new String('JS');
+const str = new String("JS");
 console.log(typeof str);
 
 //  object
 
 // calling new always make an object instance
+
+// =========>>> Comparison <<<=====================
+
+const str_1 = "JS";
+console.log(typeof(str_1));
+// string
+
+console.log(typeof str == typeof str_1);
+// false
