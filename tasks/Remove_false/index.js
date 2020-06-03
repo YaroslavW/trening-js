6ce297a550bb99b59e691783f576aac24c471dc3
Yaroslav <doctor355@yahoo.com>
2020-06-03 11:12:00 +0300

    filter arr

--- tasks/Remove_false/index.js
+++ tasks/Remove_false/index.js
@@ -4,3 +4,7 @@ let arr = [0, 1, "", undefined, false, 2, undefined, null, 3, NaN];
 let res_1 = arr.filter(Boolean);
 console.log(res_1);
 // [1, 2, 3];
+
+let res_2 = arr.filter((x) => !!x);
+console.log(res_2);
+// [1, 2, 3];
