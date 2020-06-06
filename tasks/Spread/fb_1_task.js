07db493eeb140db91b68a0483ece68e8d146e1b0
Yaroslav <doctor355@yahoo.com>
2020-06-06 09:22:00 +0300

    some func as arrow func

--- tasks/Spread/fb_1_task.js
+++ tasks/Spread/fb_1_task.js
@@ -5,3 +5,14 @@ function testF(...arg1) {
 }
 console.log(testF(1, 2, 3, 4, 5));
 // [ 3, 4, 5, 6, 7 ]
+
+// ===================================>>> As Arrow <<<======================
+
+const testFA = (...arg1) => {
+  return arg1.map((a) => {
+    return a + 2;
+  });
+};
+
+console.log(testFA(1, 2, 3, 4, 5));
+// [ 3, 4, 5, 6, 7 ]
