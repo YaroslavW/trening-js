5beb4197484ecd60ad0663d5b1db87efd80331ed
Yaroslav <doctor355@yahoo.com>
2020-06-18 20:57:16 +0300

    typescript - simple ex

---
+++ TypeScript/1/greeter.js
@@ -0,0 +1,14 @@
+var Student = /** @class */ (function () {
+    function Student(firstName, middleName, lastName) {
+        this.firstName = firstName;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.fullName = firstName + " " + middleName + " " + lastName;
+    }
+    return Student;
+}());
+function greeter(person) {
+    return "Hello " + person.firstName + " " + person.lastName;
+}
+var user = new Student("Jane", "M.", "User");
+document.body.textContent = greeter(user);
