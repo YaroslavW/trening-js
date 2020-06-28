c40210ee67fe32c84b7987459fc981fd5f197e8b
Yaroslav <doctor355@yahoo.com>
2020-06-28 09:42:47 +0300

    simpe example with var for - loop and setTimeout

---
+++ FB-tasks/for_with_setTimeout/index_1.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 200);
+}
+// 3;
+// 3;
+// 3;
+for (let i = 0; i < 3; i++) {
+  setTimeout(function () {
+    console.log(i);
+  }, 200);
+}
