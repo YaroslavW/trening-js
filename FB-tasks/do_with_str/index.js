ee443443a7f6f8476785db71b6ffe41d14317101
Yaroslav <doctor355@yahoo.com>
2020-06-29 08:20:00 +0300

    addition and subtraction with string

---
+++ FB-tasks/do_with_str/index.js
@@ -0,0 +1,4 @@
+let sub = "5" - 2;
+console.log(sub);
+let add = 2 + 3 + "5";
+console.log(add);
--- FB-tasks/this/index.js
+++ FB-tasks/this/index.js
@@ -1,7 +1,9 @@
 var name = "Java";
 this.name = "Script";
 console.log(name);
+// Java
 
 let fullName = "JS";
 this.fullName = "JavaScript";
 console.log(fullName);
+// JS
