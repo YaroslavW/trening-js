3d247f384b8d066a3535a59443a22b65b0a817ee
Yaroslav <doctor355@yahoo.com>
2020-06-30 14:07:57 +0300

    simple geim with prompt and money calculate

---
+++ FB-tasks/Geim-How_much_$_need_to_living/index.js
@@ -0,0 +1,24 @@
+function addData() {
+  const age = prompt("Type your age");
+  // console.log(age);
+  const elAge = document.getElementById("age");
+  const elMoney = document.getElementById("money");
+  elAge.innerHTML = age;
+  let money = null;
+  let number = Math.random() * 10 + 1;
+  if (age < 10) {
+    money = Math.round(age / number + 85);
+  } else if (age >= 10 && age <= 50) {
+    money = Math.round(age / number + 60);
+  } else {
+    money = Math.round(age / number + 30);
+  }
+  return (elMoney.innerHTML = money);
+}
+
+const check = () => {
+  console.log(`age = ${window.age}`);
+  console.log(`money = ${window.money}`);
+  console.log(`elAge = ${window.elAge}`);
+  console.log(`elMoney = ${window.elMoney}`);
+};
