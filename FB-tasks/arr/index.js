c8e03a6ca8e4dca344e90f320ac37d01c56f9006
Yaroslav <doctor355@yahoo.com>
2020-07-01 08:12:15 +0300

    simple example as sort() works

---
+++ FB-tasks/arr/index.js
@@ -0,0 +1,7 @@
+var calc = [50, 70, 20, 90];
+calc.sort();
+console.log(calc);
+
+var newCalc = calc[1] + 20 + calc[1];
+console.log(newCalc);
+// 120
--- FB-tasks/do_with_str/index.js
+++ FB-tasks/do_with_str/index.js
@@ -1,4 +1,6 @@
 let sub = "5" - 2;
 console.log(sub);
+// 3
 let add = 2 + 3 + "5";
 console.log(add);
+// 55
