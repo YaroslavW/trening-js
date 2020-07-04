a51d515e807eb7e02018a2f6defaa4a5d086c83c
Yaroslav <doctor355@yahoo.com>
2020-07-04 09:25:01 +0300

    method sort without arg

---
+++ Tricks/sort_arr/index.js
@@ -0,0 +1,17 @@
+// task
+let arr = [4, 2, 8, 6, 10];
+
+console.log(arr[0]); //4
+
+console.log(arr.sort()[0]); // ?
+
+console.log(arr[0]); // ?
+
+// =======>>>
+console.log(arr);
+// [ 10, 2, 4, 6, 8 ]
+var numberArray = [40, 1, 5, 200, 10];
+numberArray.sort();
+console.log(numberArray);
+// [(1, 10, 200, 40, 5)];
+// По умолчанию элементы сортируются как строки.
