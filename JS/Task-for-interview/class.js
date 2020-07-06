b552cf5ec8704a560c2b78ab21d8a4133a0e973c
Yaroslav <doctor355@yahoo.com>
2020-07-06 21:00:31 +0300

    constructor in class

--- JS/Task-for-interview/class.js
+++ JS/Task-for-interview/class.js
@@ -1,3 +1,4 @@
+// constructor for create obj
 function Customer(name, phone) {
   this.name = name;
   this.phone = phone;
@@ -13,3 +14,24 @@ for (let a in Ivanov) {
 }
 // This customer has : name = Ivan
 // This customer has : phone = +380055123-45-24
+
+// ======>>> class <<<=============
+
+class Programmer {
+  constructor(name, phone) {
+    this.name = name;
+    this.phone = phone;
+  }
+  seyHello() {
+    console.log(`Hello! I am ${this.name}`);
+  }
+}
+
+let Kolesnikov = new Programmer("Yaroslav", "+380055-123-44-55");
+
+console.log(
+  `New programmer name is - ${Kolesnikov.name}, phone = ${Kolesnikov.phone}`
+);
+Kolesnikov.seyHello();
+// New programmer name is - Yaroslav, phone = +380055-123-44-55
+// Hello! I am Yaroslav
