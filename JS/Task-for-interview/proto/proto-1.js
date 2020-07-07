d364c062563adc216d59b9f73f38c9fd0e85fc33
Yaroslav <doctor355@yahoo.com>
2020-07-07 09:24:07 +0300

    example proto

--- JS/Task-for-interview/proto/proto-1.js
+++ JS/Task-for-interview/proto/proto-1.js
@@ -9,6 +9,29 @@ let car = {
 let bigCar = {
   weight: 3000,
   __proto__: car,
+  speed: 90,
+};
+
+const smallCar = {
+  weight: 500,
+  speed: 20,
+  __proto__: car,
 };
 
 bigCar.voise();
+// Fa-Fa-fa
+console.log(bigCar.speed);
+
+smallCar.voise();
+// Fa-Fa-fa
+console.log(smallCar.weight);
+// 500
+
+console.log(bigCar.weight);
+// 3000
+
+console.log(smallCar.speed);
+// 20
+
+console.log(bigCar.speed);
+// 90
