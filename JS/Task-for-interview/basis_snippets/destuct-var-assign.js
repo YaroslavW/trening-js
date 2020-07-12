40c5ccd360ffcc50a0258771610fba56beb783cf
Yaroslav <doctor355@yahoo.com>
2020-07-12 09:03:06 +0300

    destructuring assignment

---
+++ JS/Task-for-interview/basis_snippets/destuct-var-assign.js
@@ -0,0 +1,10 @@
+let profile = ["bob", 34, "carpenter"];
+
+let [name, age, job] = profile;
+
+console.log(`name = ${name}`);
+console.log(`age = ${age}`);
+console.log(`job = ${job}`);
+// name = bob
+// age = 34
+// job = carpenter
