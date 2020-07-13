00f8e1268a8c08b3635cfbd14506702775f0d189
Yaroslav <doctor355@yahoo.com>
2020-07-13 10:17:11 +0300

    arr to obj pro method with spread operator

--- FB-tasks/arr/arr-to-obj/index.js
+++ FB-tasks/arr/arr-to-obj/index.js
@@ -1,6 +1,7 @@
 const arr = ["name", "age", "job", 45];
 const obj = {};
 
+// =================================================>>> AMATEUR <=========
 for (let i = 0; i < arr.length; i++) {
   if (arr[i] !== undefined) {
     obj[i] = arr[i];
@@ -9,3 +10,9 @@ for (let i = 0; i < arr.length; i++) {
 console.log(obj);
 // { '0': 'name', '1': 'age', '2': 'job', '3': 45 }
 
+// ==============================================>>> PRO <<<=============
+const arr_1 = ["name", "age", "job", 45];
+const obj_1 = { ...arr };
+
+console.log(obj_1)
+// { '0': 'name', '1': 'age', '2': 'job', '3': 45 }
