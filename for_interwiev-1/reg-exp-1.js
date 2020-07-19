f5319bdb96c49afaaba485e85394de7aece3b507
Yaroslav <doctor355@yahoo.com>
2020-07-19 08:32:00 +0300

    same example with es6 sintacsis

--- for_interwiev-1/reg-exp-1.js
+++ for_interwiev-1/reg-exp-1.js
@@ -8,3 +8,13 @@ else console.log("найдено"); // будет найдено
 if (str2.search(a) == -1) console.log("не найдено");
 // будет не найдено
 else console.log("найдено");
+
+const a_1 = /Yaroslav/;
+const str_1 = "Kolesnikov Yaroslav";
+const str_2 = "Kolesnikov Jaroslav";
+
+const ans_1 = str_1.search(a_1) === -1 ? "No" : "Yes";
+console.log(ans_1);
+// No
+const ans_2 = str_2.search(a_1) === -1 ? "no" : " yes";
+console.log(ans_2);
