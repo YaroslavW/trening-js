45cf1321400c774adb933a58878989be4be2c753
Yaroslav <doctor355@yahoo.com>
2020-07-20 18:10:01 +0300

    added with in console as timer

--- block-codes_for_site/script.js
+++ block-codes_for_site/script.js
@@ -1,3 +1,8 @@
+// progressbar has tp reach 100% in 3000ms
+// width = 500 height=30px
+
+// 3000/100 = 30ms
+
 const container = document.querySelector(".container");
 const inner = document.createElement("div");
 container.appendChild(inner);
@@ -7,6 +12,8 @@ inner.style.height = "inherit";
 inner.style.background = "blue";
 
 // debugger;
+let width = 0;
 setInterval(() => {
-  console.log();
-}, 1000);
+  width++;
+  console.log(width);
+}, 30);
