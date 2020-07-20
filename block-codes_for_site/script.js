951c56e4f294a2ea8d8d07b9e3a90b86c5f46fb5
Yaroslav <doctor355@yahoo.com>
2020-07-20 18:00:32 +0300

    progress bar simple example start

---
+++ block-codes_for_site/script.js
@@ -0,0 +1,9 @@
+const container = document.querySelector(".container");
+const inner = document.createElement("div");
+container.appendChild(inner);
+
+inner.style.width = "0px";
+inner.style.height = "inherit";
+inner.style.background = "blue";
+
+// debugger;
