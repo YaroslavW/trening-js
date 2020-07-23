7a0fc35187bcc8b414191a15725ed5adb2355072
Yaroslav <doctor355@yahoo.com>
2020-07-23 09:32:00 +0300

    change style-files in 5 sec

--- css-and-layouts/timeOutLoade/script.js
+++ css-and-layouts/timeOutLoade/script.js
@@ -1,1 +1,13 @@
 
+
+window.onload = function(){
+  setTimeout(changeStyles, 5000)
+}
+
+function changeStyles(){
+  const style = document.createElement("link");
+  style.rel = "stylesheet";
+  style.href = "style2.css";
+
+  document.body.appendChild(style);
+}
