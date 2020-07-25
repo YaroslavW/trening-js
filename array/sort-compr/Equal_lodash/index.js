2ba74f18cb6b9fc27747f3b3a0d1a916a46218ff
Yaroslav <doctor355@yahoo.com>
2020-07-25 08:43:46 +0300

    comparing after sort

--- array/sort-compr/Equal_lodash/index.js
+++ array/sort-compr/Equal_lodash/index.js
@@ -13,3 +13,10 @@ const t1 = [4, 2, 3, 1];
 const equal = _.isEqual(t, t1);
 console.log(`array-t and array t1 is equal ? = ${equal}`);
 // array-t and array t1 is equal ? = false
+
+const p = [2, 4, 3, 1].sort();
+const p1 = [4, 2, 3, 1].sort();
+
+const equal1 = _.isEqual(p, p1);
+console.log(`array-t and array t1 is equal ? = ${equal1}`);
+// array-t and array t1 is equal ? = true
