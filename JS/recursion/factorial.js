97d0345498a0143f8e983fb88c5e0b1aeb1935af
Yaroslav <doctor355@yahoo.com>
2020-08-05 15:21:12 +0300

    fac as ex

--- JS/recursion/factorial.js
+++ JS/recursion/factorial.js
@@ -17,3 +17,15 @@ function factorial(n) {
 }
 
 console.log(factorial(5)); // 120
+
+
+function factorialize(num) {
+  var result = num;
+  if (num === 0 || num === 1) return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
+  }
+  return result;
+}
+factorialize(5);
