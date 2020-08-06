3208c97b8dea1e74ed5e8a1c19ab65b168fafbbe
Yaroslav <doctor355@yahoo.com>
2020-08-06 08:23:00 +0300

    fb-task added int and str

---
+++ FB-tasks/int_plus_str/index.js
@@ -0,0 +1,10 @@
+let x = 1 + "1";
+let y = 1 + 1;
+let z = "1" + "1";
+console.log(`x = ${x}`);
+console.log("typeof x = " + typeof x);
+if (x === 2) {
+  console.log(y);
+} else {
+  console.log(z);
+}
