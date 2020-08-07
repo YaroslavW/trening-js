0fffbf83901eb80e025635e8c1b0e5667cc5f4c1
Yaroslav <doctor355@yahoo.com>
2020-08-07 09:12:00 +0300

    random g

---
+++ geym-random/index.js
@@ -0,0 +1,22 @@
+function geym() {
+  const random = Math.random() * 10;
+  console.log(random);
+  if (random <= 4) {
+    console.log("РИС");
+  } else if (4 < random && random <= 7) {
+    console.log("КАРТОШКА");
+  } else {
+    console.log("КУКУРУЗА");
+  }
+}
+geym();
+geym();
+geym();
+geym();
+geym();
+geym();
+geym();
+geym();
+geym();
+geym();
+geym();
