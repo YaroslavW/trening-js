2ba2b5a6226779677ef9ac002c7e00c816b6c9d2
Yaroslav <doctor355@yahoo.com>
2020-08-08 12:21:00 +0300

    added arrow function

--- geym-random/index.js
+++ geym-random/index.js
@@ -9,14 +9,39 @@ function geym() {
     console.log("КУКУРУЗА");
   }
 }
-geym();
-geym();
-geym();
-geym();
-geym();
-geym();
-geym();
-geym();
-geym();
-geym();
-geym();
+// geym();
+// geym();
+// geym();
+// geym();
+// geym();
+// geym();
+// geym();
+// geym();
+// geym();
+// geym();
+// geym();
+
+const geim1 = () => {
+  const random = Math.floor(Math.random() * Math.floor(3));
+  switch (random) {
+    case 0:
+      console.log("РИС");
+      break;
+    case 1:
+      console.log("КАРТОШКА");
+      break;
+    case 2:
+      console.log("КУКУРУЗА");
+      break;
+    default:
+      console.log("err", err);
+  }
+};
+geim1();
+geim1();
+geim1();
+geim1();
+geim1();
+geim1();
+geim1();
+geim1();
