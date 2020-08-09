60f54f7498767d3dd87618ae628ae6d02117f2c4
Yaroslav <doctor355@yahoo.com>
2020-08-09 10:34:00 +0300

    Another way to sort an array :-)

---
+++ sort_timeout/index.js
@@ -0,0 +1,20 @@
+const arr = [20, 5, 100, 1, 4, 22, 34, 987, 58, 43, 12];
+
+const sort = (array) => {
+  for (let i of array) {
+    setTimeout(() => console.log(i), i);
+  }
+};
+
+sort(arr);
+// 1;
+// 4;
+// 5;
+// 12;
+// 20;
+// 22;
+// 34;
+// 43;
+// 58;
+// 100;
+// 987;
