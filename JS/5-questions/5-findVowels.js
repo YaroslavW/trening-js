b477639f9bd5f5cf2a222967ad5286dc9112666e
Yaroslav <doctor355@yahoo.com>
2020-08-16 20:20:45 +0300

    find fowels classic

---
+++ JS/5-questions/5-findVowels.js
@@ -0,0 +1,13 @@
+// Нужно написать функцию, принимающую строку в качестве аргумента и возвращающую количество гласных, которые содержатся в строке.
+// Гласными являются «a», «e», «i», «o», «u».
+
+const findVowels = (str) => {
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
+  }
+  return count;
+};
