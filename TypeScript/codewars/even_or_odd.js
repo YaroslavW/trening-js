6ed313e73b49e506c074164ae0501e342d34ae89
Yaroslav <doctor355@yahoo.com>
2020-09-11 10:28:51 +0300

    other solution

--- TypeScript/codewars/even_or_odd.js
+++ TypeScript/codewars/even_or_odd.js
@@ -2,12 +2,12 @@
 exports.__esModule = true;
 exports.even_or_odd = void 0;
 function even_or_odd(n) {
-    if (n % 2 === 0) {
-        return "Even";
-    }
-    else {
-        return "Odd";
-    }
+  // if (n % 2 === 0) {
+  //   return "Even";
+  // } else {
+  //   return "Odd";
+  // }
+  return n % 2 === 0 ? "Even" : "Odd";
 }
 exports.even_or_odd = even_or_odd;
 console.log(even_or_odd(55));
--- TypeScript/codewars/even_or_odd.ts
+++ TypeScript/codewars/even_or_odd.ts
@@ -1,9 +1,10 @@
 export function even_or_odd(n: number): string {
-  if (n % 2 === 0) {
-    return "Even";
-  } else {
-    return "Odd";
-  }
+  // if (n % 2 === 0) {
+  //   return "Even";
+  // } else {
+  //   return "Odd";
+  // }
+  return n % 2 === 0 ? 'Even':'Odd'
 }
 
 console.log(even_or_odd(55));
