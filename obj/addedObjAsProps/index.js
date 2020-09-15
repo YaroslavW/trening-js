d306f3375c251e73c04e4e57849b01c7badc8c08
Yaroslav <doctor355@yahoo.com>
2020-09-15 15:51:12 +0300

    Added object as property in another object

---
+++ obj/addedObjAsProps/index.js
@@ -0,0 +1,53 @@
+const tagList = [
+  {
+    id: 1,
+    title: "javascript",
+  },
+  {
+    id: 2,
+    title: "css",
+  },
+];
+
+const postList = [
+  {
+    id: 1,
+    title: "title-1",
+    tag_ids: "1, 2",
+  },
+  {
+    id: 2,
+    title: "title-2",
+    tag_ids: "1",
+  },
+];
+// How to transform above data to this below
+const res_1 = [
+  {
+    id: 1,
+    title: "javascript",
+    posts: [
+      { id: 1, title: "title-1" },
+      { id: 2, title: "title-2" },
+    ],
+  },
+  {
+    id: 1,
+    title: "css",
+    posts: [{ id: 1, title: "title-1" }],
+  },
+];
+
+// solution
+
+const postListWithTagsArray = postList.map((post) => ({
+  ...post,
+  tag_ids: tag_ids.split(",").map((tag_Id) => parseInt(tag_Id, 10)),
+}));
+
+const res = tagList.map((tag) => ({
+  ...tag,
+  posts: postListWithTagsArray.filter((post) => post.tag_ids.includes(tag.id)),
+}));
+
+console.log(res);
