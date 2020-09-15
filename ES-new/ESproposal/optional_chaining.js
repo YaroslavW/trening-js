7967ee43d606a94cf8ccef342c6c3def13f55732
Yaroslav <doctor355@yahoo.com>
2020-09-15 08:30:07 +0300

    optional chaining old method

--- ES-new/ESproposal/optional_chaining.js
+++ ES-new/ESproposal/optional_chaining.js
@@ -1,8 +1,8 @@
 const data = {
   user: {
     address: {
-      street: 'Pennsylvania Avenue',
-    }, 
+      street: "Pennsylvania Avenue",
+    },
   },
 };
 // or
@@ -10,3 +10,7 @@ const data = {
 //   user: {},
 // };
 console.log(data.user.address.street); // Uncaught TypeError: Cannot read property 'street' of undefined
+
+const street =
+  data && data.user && data.user.address && data.user.address.street;
+console.log(street); // undefined
