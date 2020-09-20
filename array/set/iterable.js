54281ba49f1ee9c60c8bd0f3bdb8eb99b73d77ca
Yaroslav <doctor355@yahoo.com>
2020-09-20 08:23:00 +0300

    try ittration Set

--- array/set/iterable.js
+++ array/set/iterable.js
@@ -3,3 +3,6 @@ const set = new Set([1, 2, 3, 4, 1, 2, 3, 4, 1, 2, 3, 4]);
 console.log(set);
 // Set(4) { 1, 2, 3, 4 }
 
+const dubble = set.map((num) => num * num);
+console.log(dubble);
+// TypeError: set.map is not a function
